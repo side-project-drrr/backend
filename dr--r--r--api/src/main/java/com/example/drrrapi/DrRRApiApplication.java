98a4f7740c35d9de1bb89524a7035d163b52d721
YoungKyonYou <man.spride1324@gmail.com>
2023-08-04 15:34:02 +0900

    initial commit

---
+++ dr--r--r--api/src/main/java/com/example/drrrapi/DrRRApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.drrrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrRRApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrRRApiApplication.class, args);
+    }
+
+}
---
+++ dr--r--r--batch/src/test/java/com/example/drrrbatch/DrRRBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drrrbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrRRBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
