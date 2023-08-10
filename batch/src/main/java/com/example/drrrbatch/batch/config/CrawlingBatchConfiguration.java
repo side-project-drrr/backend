8ccac9365bad74b4db8f6ee821d2e3ea26bad5a2
JaehongDev <println@kakao.com>
2023-08-10 15:24:16 +0900

    feat: 기술블로그 크롤링 후 임시 테이블 저장 기능 구현

--- batch/src/main/java/com/example/drrrbatch/batch/config/CrawlingBatchConfiguration.java
+++ batch/src/main/java/com/example/drrrbatch/batch/config/CrawlingBatchConfiguration.java
@@ -1,6 +1,11 @@
 package com.example.drrrbatch.batch.config;
 
 
+import com.example.drrrbatch.batch.domain.ExternalBlogPosts;
+import com.example.drrrbatch.batch.entity.TemporalTechBlogPost;
+import com.example.drrrbatch.batch.reader.CrawlerItemReaderFactory;
+import com.example.drrrbatch.batch.repository.TemporalTechBlogPostRepository;
+import com.example.drrrbatch.batch.vo.TechBlogCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -10,7 +15,7 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.Chunk;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +25,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @RequiredArgsConstructor
 public class CrawlingBatchConfiguration {
-    private static final String BATCH_NAME = "Crawling";
+    private static final String BATCH_NAME = "crawling";
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
+    private final CrawlerItemReaderFactory crawlerItemReaderFactory;
+    private final TemporalTechBlogPostRepository temporalTechBlogPostRepository;
 
-    @Bean(name = BATCH_NAME + "job")
+    @Bean(name = BATCH_NAME + "Job")
     public Job crawlingJob() {
         return new JobBuilder(BATCH_NAME + "Job", jobRepository)
                 .incrementer(new RunIdIncrementer())
@@ -33,18 +40,34 @@ public class CrawlingBatchConfiguration {
                 .build();
     }
 
-    @Bean(name = BATCH_NAME + "step")
+    @Bean(name = BATCH_NAME + "Step")
     @JobScope
     public Step crawlingStep(
             @Value("#{jobParameters[techBlogCode]}") Long code
     ) {
         log.info("get parameter code: {}", code);
         return new StepBuilder(BATCH_NAME + "Step", jobRepository)
-                .tasklet(((contribution, chunkContext) -> {
-                    log.info(BATCH_NAME + " run ");
-                    return RepeatStatus.FINISHED;
-                }), transactionManager)
+                .<ExternalBlogPosts, ExternalBlogPosts>chunk(1, transactionManager)
+                .reader(crawlerItemReaderFactory.createItemReader(TechBlogCode.valueOf(code)))
+                .writer(this::saveChunkToTemporalTechBlogRepository)
                 .build();
+    }
+
+    private void saveChunkToTemporalTechBlogRepository(Chunk<? extends ExternalBlogPosts> chunk) {
+        this.temporalTechBlogPostRepository.saveAll(chunk.getItems()
+                .stream()
+                .flatMap(externalPosts -> externalPosts.posts()
+                        .stream()
+                        .map(post -> TemporalTechBlogPost.builder()
+                                .title(post.title())
+                                .author(post.author())
+                                .summary(post.summary())
+                                .thumbnailUrl(post.thumbnailUrl())
+                                .url(post.link())
+                                .createdDate(post.postDate())
+                                .techBlogCode(post.code())
+                                .urlSuffix(post.suffix()).build())
+                ).toList());
     }
 
 }
--- batch/src/main/java/com/example/drrrbatch/batch/reader/CrawlerItemReaderFactory.java
+++ batch/src/main/java/com/example/drrrbatch/batch/reader/CrawlerItemReaderFactory.java
@@ -1,23 +1,29 @@
 package com.example.drrrbatch.batch.reader;
 
+import com.example.drrrbatch.batch.reader.impl.MarketKurlyItemReader;
+import com.example.drrrbatch.batch.reader.impl.NaverCrawlerItemReader;
 import com.example.drrrbatch.batch.reader.impl.TestCrawlerPageItemReader;
 import com.example.drrrbatch.batch.vo.TechBlogCode;
 import java.util.function.Function;
-import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CrawlerItemReaderFactory {
 
-    public AbstractCrawlerPageItemReader createItemReader(TechBlogCode code, Supplier<WebDriver> supplier) {
-        return this.createItemReader(code).apply(supplier.get());
+    private final WebDriver webDriver;
+
+    public AbstractCrawlerPageItemReader createItemReader(TechBlogCode code) {
+        return this.findItemReaderBy(code).apply(webDriver);
     }
 
-    public Function<WebDriver, AbstractCrawlerPageItemReader> createItemReader(TechBlogCode code) {
+    public Function<WebDriver, AbstractCrawlerPageItemReader> findItemReaderBy(TechBlogCode code) {
         return switch (code) {
             case BASE -> TestCrawlerPageItemReader::new;
-            case MARKET_KURLY, NAVER -> null;
+            case MARKET_KURLY -> MarketKurlyItemReader::new;
+            case NAVER -> NaverCrawlerItemReader::new;
         };
     }
 }
