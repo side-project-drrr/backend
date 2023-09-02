90091b5a9d8dea571381daa28c9c28e7b3815cca
YoungKyonYou <man.spride1324@gmail.com>
2023-09-02 16:24:49 +0900

    chore: httpClient 추가 및 스프링 버전 6.1로 변경, 도커 컴포즈 추가

--- api/src/main/java/com/example/drrrapi/DrRRApiApplication.java
+++ api/src/main/java/com/example/drrrapi/DrRRApiApplication.java
@@ -1,12 +1,22 @@
 package com.example.drrrapi;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DrRRApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DrRRApiApplication.class, args);
+    }
+    /**
+     * 서버 타임존 설정
+     */
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
