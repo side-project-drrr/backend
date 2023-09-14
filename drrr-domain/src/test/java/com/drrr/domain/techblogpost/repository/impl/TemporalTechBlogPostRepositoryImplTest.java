a2ffbf4cd37ee0b7bbb7666639ce57af32024ce2
JaehongDev <println@kakao.com>
2023-09-14 22:43:32 +0900

    test: 임시 기술블로그 테스트 코드 변경

--- drrr-domain/src/test/java/com/drrr/domain/techblogpost/repository/impl/TemporalTechBlogPostRepositoryImplTest.java
+++ drrr-domain/src/test/java/com/drrr/domain/techblogpost/repository/impl/TemporalTechBlogPostRepositoryImplTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.drrr.core.code.TechBlogCode;
 import com.drrr.core.date.DateRangeBound;
+import com.drrr.domain.annotation.EnableMysqlProfile;
 import com.drrr.domain.annotation.EnableRepositoryTest;
 import com.drrr.domain.category.entity.Category;
 import com.drrr.domain.category.repository.CategoryRepository;
@@ -13,12 +14,13 @@ import com.drrr.domain.techblogpost.repository.TemporalTechBlogPostRepository;
 import com.drrr.domain.techblogpost.repository.TemporalTechPostTagRepository;
 import java.time.LocalDate;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @EnableRepositoryTest
+@EnableMysqlProfile
 class TemporalTechBlogPostRepositoryImplTest {
     @Autowired
     private TemporalTechBlogPostRepository temporalTechBlogPostRepository;
@@ -29,53 +31,45 @@ class TemporalTechBlogPostRepositoryImplTest {
     @Autowired
     private TemporalTechPostTagRepository temporalTechPostTagRepository;
 
-    @Test
-    void 크롤링된_당일_게시글을_정상적으로_불러옵니다() {
+    /**
+     * <h3>Given</h3>
+     * <br>2개의 카테고리(Java Node.js)가 등록되어 있습니다
+     * <br> 2개의 게시글이 등록되어 있습니다.
+     * <br> 여기서 게시글 하나는 현재 일자 기준으로 하루 전에 등록된 게시글이고
+     * <br> 다른 하나는 현재 일자 기준으로 등록된 데이터 입니다.
+     * <br> 게시글의 카테고리는 오직 1번 게시글에만 등록되어 있으며 (JAVA,Node.js) 두 카테고리를 가지고 있습니다.
+     */
+    @BeforeEach
+    void setup() {
         final var JAVACategory = categoryRepository.saveAndFlush(Category.builder()
                 .displayName("자바")
                 .uniqueName("JAVA")
                 .build());
-
-        final var post1 = temporalTechBlogPostRepository.saveAndFlush(TemporalTechBlogPost.builder()
-                .author("author")
-                .createdDate(LocalDate.now())
-                .thumbnailUrl(null)
-                .title("title")
-                .summary("summary")
-                .url("")
-                .urlSuffix("suffix")
-                .techBlogCode(TechBlogCode.BASE)
-                .crawledDate(LocalDate.now())
-                .build());
-
-        final var temporalTechPostTag = temporalTechPostTagRepository.saveAllAndFlush(List.of(new TemporalTechPostTag(JAVACategory, post1)));
-        post1.registerCategory(temporalTechPostTag);
-
-        final var result = temporalTechBlogPostRepository.findBy(DateRangeBound.builder()
-                .startDate(LocalDate.now())
-                .lastDate(LocalDate.now())
-                .build()
-        );
-
-        assertThat(post1).isEqualTo(result.get(0));
-        assertThat(post1.getTemporalTechPostTags().get(0)).isEqualTo(temporalTechPostTag.get(0));
-    }
-
-
-    @Test
-    void 크롤링된_게시글_중에서_기간내_존재하지_않는_글은_가져오지_않는다() {
-        final var JAVACategory = categoryRepository.save(Category.builder()
-                .displayName("자바")
-                .uniqueName("JAVA")
-                .build());
-
         final var NodeCategory = categoryRepository.save(Category.builder()
                 .displayName("node js")
                 .uniqueName("NODE_JS")
                 .build());
 
-        // 당일 게시글
-        final var post1 = temporalTechBlogPostRepository.save(TemporalTechBlogPost.builder()
+        final var post = temporalTechBlogPostRepository.saveAndFlush(TemporalTechBlogPost.builder()
+                .author("author")
+                .createdDate(LocalDate.now())
+                .thumbnailUrl(null)
+                .title("title")
+                .summary("summary")
+                .url("")
+                .urlSuffix("suffix")
+                .techBlogCode(TechBlogCode.BASE)
+                .crawledDate(LocalDate.now().minusDays(1))
+                .build());
+
+        final var temporalTechPostTag = temporalTechPostTagRepository.saveAll(List.of(
+                new TemporalTechPostTag(JAVACategory, post),
+                new TemporalTechPostTag(NodeCategory, post)
+        ));
+
+        post.registerCategory(temporalTechPostTag);
+
+        temporalTechBlogPostRepository.save(TemporalTechBlogPost.builder()
                 .author("author")
                 .createdDate(LocalDate.now())
                 .thumbnailUrl(null)
@@ -86,55 +80,38 @@ class TemporalTechBlogPostRepositoryImplTest {
                 .techBlogCode(TechBlogCode.BASE)
                 .crawledDate(LocalDate.now())
                 .build());
+    }
 
-        final var temporalNodeTag = temporalTechPostTagRepository.save(new TemporalTechPostTag(NodeCategory, post1));
-        final var temporalJavaTag = temporalTechPostTagRepository.save(new TemporalTechPostTag(JAVACategory, post1));
+    @Test
+    void 기간_상관없이_전체_게시글을_가져옵니다() {
+        final var result = temporalTechBlogPostRepository.findBy(null, (Boolean) null);
 
-        post1.registerCategory(List.of(temporalNodeTag, temporalJavaTag));
+        assertThat(result).size().isEqualTo(2);
+    }
 
+
+    @Test
+    void 크롤링한_날짜_기준으로_하루_전_게시글을_조회한다() {
         // 현재 일자 기준으로 1~2일 지난 게시글 조회
         final var result = temporalTechBlogPostRepository.findBy(DateRangeBound.builder()
-                .startDate(LocalDate.now().minusDays(2))
+                .startDate(LocalDate.now().minusDays(1))
                 .lastDate(LocalDate.now().minusDays(1))
-                .build());
+                .build(), null);
 
-        Assertions.assertThat(result).isEmpty();
+        assertThat(result).size().isEqualTo(1);
     }
 
     @Test
-    void 크롤링된_게시글_중에서_카테고리가_여러개_인_글을_긁어온다() {
-        final var JAVACategory = categoryRepository.save(Category.builder()
-                .displayName("자바")
-                .uniqueName("JAVA")
-                .build());
+    void 크롤링된_게시글을_가져올_때_카테고리_또한_가져온다() {
+        final var result = temporalTechBlogPostRepository.findBy(DateRangeBound.createSingleRange(LocalDate.now().minusDays(1)), null);
 
-        final var NodeCategory = categoryRepository.save(Category.builder()
-                .displayName("node js")
-                .uniqueName("NODE_JS")
-                .build());
+        assertThat(result).size().isEqualTo(1);
+        assertThat(result.get(0).getTemporalTechPostTags()).size().isEqualTo(2);
+    }
 
-        // 당일 게시글
-        final var post1 = temporalTechBlogPostRepository.save(TemporalTechBlogPost.builder()
-                .author("author")
-                .createdDate(LocalDate.now())
-                .thumbnailUrl(null)
-                .title("title")
-                .summary("summary")
-                .url("")
-                .urlSuffix("suffix")
-                .techBlogCode(TechBlogCode.BASE)
-                .crawledDate(LocalDate.now())
-                .build());
-
-        final var temporalNodeTag = temporalTechPostTagRepository.save(new TemporalTechPostTag(NodeCategory, post1));
-        final var temporalJavaTag = temporalTechPostTagRepository.save(new TemporalTechPostTag(JAVACategory, post1));
-
-        post1.registerCategory(List.of(temporalNodeTag, temporalJavaTag));
-
-        // 현재 일자 기준으로 1~2일 지난 게시글 조회
-        final var result = temporalTechBlogPostRepository.findBy(DateRangeBound.createSingleRange(LocalDate.now()));
-
-        Assertions.assertThat(result).size().isEqualTo(1);
-        Assertions.assertThat(result.get(0).getTemporalTechPostTags()).size().isEqualTo(2);
+    @Test
+    void 크롤링된_게시글의_태그가_등록이_완료된_게시글만_조회한다() {
+        final List<TemporalTechBlogPost> temporalTechBlogPosts = temporalTechBlogPostRepository.findBy(null, true);
+        assertThat(temporalTechBlogPosts).size().isEqualTo(1);
     }
 }
