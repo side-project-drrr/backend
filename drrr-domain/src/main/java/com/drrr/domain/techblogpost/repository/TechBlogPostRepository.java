d5ae1301b1800256fa54717bf5e846de9cbd6d82
YoungKyonYou <man.spride1324@gmail.com>
2023-09-17 20:38:01 +0900

    fix: @Repository 어노테이션 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostRepository.java
@@ -3,7 +3,9 @@ package com.drrr.domain.techblogpost.repository;
 import com.drrr.core.code.TechBlogCode;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TechBlogPostRepository extends JpaRepository<TechBlogPost, Long> {
     boolean existsByTechBlogCodeAndUrlSuffix(TechBlogCode techBlogCode, String urlSuffix);
 }
