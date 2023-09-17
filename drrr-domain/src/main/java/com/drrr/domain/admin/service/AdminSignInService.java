0a3531dda125c1eea5182b5f8d0e6a0e3f3fb89b
JaehongDev <println@kakao.com>
2023-09-18 01:49:55 +0900

    refactor: Admin 관련 예외 제거

--- drrr-domain/src/main/java/com/drrr/domain/admin/service/AdminSignInService.java
+++ drrr-domain/src/main/java/com/drrr/domain/admin/service/AdminSignInService.java
@@ -1,7 +1,6 @@
 package com.drrr.domain.admin.service;
 
 
-import com.drrr.core.exception.admin.AdminExceptionCode;
 import com.drrr.domain.admin.entity.Admin;
 import com.drrr.domain.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +15,7 @@ public class AdminSignInService {
 
     public Long execute(final AdminSignInDto adminSignInDto) {
         final Admin admin = adminRepository.findByLoginId(adminSignInDto.loginId)
-                .orElseThrow(AdminExceptionCode.FAIL_SIGNIN::invoke);
+                .orElseThrow(IllegalArgumentException::new);
 
         admin.validateSamePassword(adminSignInDto.password());
 
