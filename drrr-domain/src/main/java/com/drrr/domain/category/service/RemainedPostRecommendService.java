2480e199e55355495da68ceac3df7d5f6cde6a26
YoungKyonYou <man.spride1324@gmail.com>
2023-09-17 22:14:55 +0900

    feat: 게시물 추천 시 더이상 추천해줄 게 없을 때 다른 게시물을 추천해줄 서비스 추가

---
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/RemainedPostRecommendService.java
@@ -0,0 +1,35 @@
+package com.drrr.domain.category.service;
+
+import static com.drrr.domain.log.entity.post.QMemberPostLog.memberPostLog;
+import static com.drrr.domain.techblogpost.entity.QTechBlogPost.techBlogPost;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RemainedPostRecommendService {
+    private JPAQueryFactory queryFactory;
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Long> recommendRemain(Long memberId, int remainedPostCount) {
+        queryFactory = new JPAQueryFactory(em);
+
+        return queryFactory.select(techBlogPost.id)
+                .from(techBlogPost)
+                .leftJoin(memberPostLog)
+                .on(
+                        memberPostLog.postId.eq(techBlogPost.id)
+                        , memberPostLog.isRead.eq(false)
+                        , memberPostLog.isRecommended.eq(false))
+                .where(memberPostLog.memberId.eq(memberId))
+                .orderBy(techBlogPost.createdDate.desc())
+                .limit(remainedPostCount)
+                .fetch();
+    }
+}
