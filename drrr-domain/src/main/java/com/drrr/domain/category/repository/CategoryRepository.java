357ee6601412533bfdc688218173a851181480d0
JaehongDev <println@kakao.com>
2023-09-18 01:52:11 +0900

    refactor: jpa 관례를 통한 id 조건으로 변경

--- drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("SELECT c FROM Category c WHERE c.id IN (:categoryIds)")
-    Optional<List<Category>> findByIds(List<Long> categoryIds);
+    List<Category> findByIdIn(List<Long> categoryIds);
 }
