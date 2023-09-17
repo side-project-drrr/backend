8ac6f7b31f7dab329b112c92586d8795c7099000
YoungKyonYou <man.spride1324@gmail.com>
2023-09-17 22:13:49 +0900

    feat: JPAQueryFactory 빈 등록 하는 설정 추가

---
+++ drrr-domain/src/main/java/com/drrr/domain/jpa/config/QuerydslConfiguration.java
@@ -0,0 +1,18 @@
+package com.drrr.domain.jpa.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfiguration {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
