18c3e055c10e58df755ef1f4714cc5b6d4ddd05e
YoungKyonYou <man.spride1324@gmail.com>
2023-09-17 22:19:06 +0900

    feat: 사용자 게시물 추천 서비스에 관한 상수 클래스 추가

---
+++ drrr-core/src/main/java/com/drrr/core/recommandation/constant/constant/DaysConstants.java
@@ -0,0 +1,13 @@
+package com.drrr.core.recommandation.constant.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum DaysConstants {
+    //특정 Days 동안 읽지 않으면 가중치를 0 또는 1로 초기화 해주기 위한 값
+    UNREAD_DAYS(3);
+    private int value;
+    DaysConstants(int value){
+        this.value = value;
+    }
+}
---
+++ drrr-core/src/main/java/com/drrr/core/recommandation/constant/constant/WeightConstants.java
@@ -0,0 +1,26 @@
+package com.drrr.core.recommandation.constant.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum WeightConstants {
+    // 최소 가중치 값
+    MIN_WEIGHT(0.0),
+    // 최대 가중치 값
+    MAX_WEIGHT(10.0),
+    // 선호하는 카테고리에 대한 최소 가중치 값
+    MIN_CONDITIONAL_WEIGHT(1.0),
+    // 감소하는 최소 가중치 값
+    DECREASE_WEIGHT(1.0),
+
+    // 증가하는 최소 가중치 값
+    INCREASE_WEIGHT(1.0);
+    private double value;
+
+    WeightConstants(double value) {
+        this.value = value;
+    }
+
+
+
+}
