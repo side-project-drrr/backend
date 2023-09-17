024e174dbdb0829d0df4ebbaa39ea652143fd0b3
YoungKyonYou <man.spride1324@gmail.com>
2023-09-17 20:08:20 +0900

    feat: 사용자가 회원가입할 시 초기 카테고리 가중치 설정하기 위한 서비스 추가

--- drrr-api/src/main/java/com/drrr/auth/service/impl/SignUpService.java
+++ drrr-api/src/main/java/com/drrr/auth/service/impl/SignUpService.java
@@ -3,6 +3,8 @@ package com.drrr.auth.service.impl;
 
 import com.drrr.auth.payload.request.SignUpRequest;
 import com.drrr.auth.payload.response.SignUpResponse;
+
+import com.drrr.domain.category.service.InitializeWeightService;
 import com.drrr.domain.member.service.RegisterMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,14 @@ public class SignUpService {
     private final ExternalAuthenticationFacade externalAuthenticationFacade;
     private final RegisterMemberService registerMemberService;
     private final IssuanceTokenService issuanceTokenService;
+    private final InitializeWeightService initializeWeightService;
 
     public SignUpResponse execute(SignUpRequest signUpRequest) {
         var socialId = externalAuthenticationFacade.execute(signUpRequest.getAccessToken(), signUpRequest.getProvider());
         var memberId = registerMemberService.execute(socialId, signUpRequest.toRegisterMemberDto());
         var issuanceTokenDto = issuanceTokenService.execute(memberId);
+        initializeWeightService.initializeCategoryWeight(memberId, signUpRequest.getCategoryIds());
+
 
         return SignUpResponse.builder()
                 .accessToken(issuanceTokenDto.getAccessToken())
