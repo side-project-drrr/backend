f3f3423b3ffc7ebd6318db4a7a991bd32a630c18
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:26:26 +0900

    feat: swagger 문서 작성 및 validation 추가

--- drrr-api/src/main/java/com/drrr/auth/payload/response/SignInResponse.java
+++ drrr-api/src/main/java/com/drrr/auth/payload/response/SignInResponse.java
@@ -1,14 +1,15 @@
 package com.drrr.auth.payload.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @Builder
-@RequiredArgsConstructor
-public class SignInResponse {
-    private final String accessToken;
-    private final String refreshToken;
-
+public record SignInResponse(
+        @Schema(description = "access token", nullable = false, example = "[access token]")
+        @NonNull String accessToken,
+        @Schema(description = "refresh token", nullable = false, example = "[refresh token]")
+        @NonNull String refreshToken) {
 }
--- drrr-api/src/main/java/com/drrr/auth/payload/response/SignUpResponse.java
+++ drrr-api/src/main/java/com/drrr/auth/payload/response/SignUpResponse.java
@@ -3,12 +3,9 @@ package com.drrr.auth.payload.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @Builder
-@RequiredArgsConstructor
-public class SignUpResponse {
-    private final String accessToken;
-    private final String refreshToken;
+public record SignUpResponse(@NonNull String accessToken, @NonNull String refreshToken) {
 }
