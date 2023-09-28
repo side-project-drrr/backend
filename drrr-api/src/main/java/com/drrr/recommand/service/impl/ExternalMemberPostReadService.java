4893866de0c46707ba1aa8fc1f556e0485798683
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:31:32 +0900

    feat: 로그 서비스 추가

--- drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalMemberPostReadService.java
+++ drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalMemberPostReadService.java
@@ -2,19 +2,25 @@ package com.drrr.recommand.service.impl;
 
 import com.drrr.domain.category.service.MemberViewWeightService;
 import com.drrr.domain.category.service.WeightValidationService;
+import com.drrr.domain.log.service.LogUpdateService;
 import com.drrr.recommand.dto.AdjustPostWeightRequest;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Service
 public class ExternalMemberPostReadService {
     private final WeightValidationService weightValidationService;
     private final MemberViewWeightService memberViewWeightService;
+    private final LogUpdateService logUpdateService;
 
-    public void execute(AdjustPostWeightRequest request) {
-        weightValidationService.validateWeight(request.getMemberId());
-        memberViewWeightService.increaseMemberViewPost(request.getMemberId(), request.getPostId(),
+    public void execute(AdjustPostWeightRequest request, Long memberId, Long postId) {
+        weightValidationService.validateWeight(memberId, LocalDateTime.now());
+        memberViewWeightService.increaseMemberViewPost(memberId, postId,
                 request.getCategoryIds());
+        logUpdateService.insertMemberPostReadLog(memberId, postId);
+        logUpdateService.insertMemberPostHistory(memberId, postId);
     }
 }
