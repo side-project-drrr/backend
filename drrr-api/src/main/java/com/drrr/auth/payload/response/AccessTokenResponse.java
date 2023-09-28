f830b05b3fa0a92d737aa5db0dc14029773a1249
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:26:06 +0900

    refactor: swagger 문서 작성

--- drrr-api/src/main/java/com/drrr/auth/payload/response/AccessTokenResponse.java
+++ drrr-api/src/main/java/com/drrr/auth/payload/response/AccessTokenResponse.java
@@ -1,13 +1,16 @@
 package com.drrr.auth.payload.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class AccessTokenResponse {
     @Schema(description = "재발급된 access token", nullable = false, example = "JWT token")
     private String accessToken;
