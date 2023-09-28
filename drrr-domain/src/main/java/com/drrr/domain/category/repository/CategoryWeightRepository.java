ca5fbe02571c04ee5921d8b828d5410c63775d5c
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:34:19 +0900

    refactor: Optional 제거

--- drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
@@ -2,7 +2,6 @@ package com.drrr.domain.category.repository;
 
 import com.drrr.domain.category.entity.CategoryWeight;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,11 +13,10 @@ public interface CategoryWeightRepository extends JpaRepository<CategoryWeight, 
     List<CategoryWeight> findCategoryWeightsByMemberId(@Param("memberId") Long memberId);
     List<CategoryWeight> findCategoryWeightByMemberId(Long memberId);
 
-   /* @Query("delete from CategoryWeight cw where cw.member.id =:memberId and cw.category.id =:categoryId")
+/*    @Query("delete from CategoryWeight cw where cw.member.id =:memberId and cw.category.id =:categoryId")
     Optional<Integer> deleteUselessCategoryWeightData(Long memberId, Long categoryId);
 
     @Query("select cw from CategoryWeight cw where cw.member.id =:memberId and cw.category.id in (:categoryIds)")
-    Optional<List<CategoryWeight>> findByMemberAndCategory(Long memberId, List<Long> categoryIds);
-*/
+    Optional<List<CategoryWeight>> findByMemberAndCategory(Long memberId, List<Long> categoryIds);*/
     List<CategoryWeight> findByMemberId(Long memberId);
 }
