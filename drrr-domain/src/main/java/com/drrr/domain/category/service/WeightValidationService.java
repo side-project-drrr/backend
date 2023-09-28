f797abbab82aaebaade9c5ba163a2a058e07008b
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:36:06 +0900

    refactor: 불필요한 import 제거 및 BaseEntity 관련 테스트를 위해서 파라미터 추가

--- drrr-domain/src/main/java/com/drrr/domain/category/service/WeightValidationService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/WeightValidationService.java
@@ -1,16 +1,9 @@
 package com.drrr.domain.category.service;
 
-import com.drrr.core.recommandation.constant.constant.DaysConstants;
-import com.drrr.core.recommandation.constant.constant.HoursConstants;
-import com.drrr.core.recommandation.constant.constant.WeightConstants;
-import com.drrr.domain.category.dto.CategoryWeightDto;
 import com.drrr.domain.category.entity.CategoryWeight;
 import com.drrr.domain.category.repository.CategoryWeightRepository;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,16 +18,16 @@ public class WeightValidationService {
     /**
      * 사용자에게 블로그 게시물을 추천해줄 때나 사용자가 특정 게시물을 읽을 때 호출
      */
-    public void validateWeight(Long memberId) {
+    public void validateWeight(Long memberId, LocalDateTime now) {
         // 가중치 회원 테이블에서 조회
         List<CategoryWeight> categoryWeights = categoryWeightRepository.findCategoryWeightsByMemberId(memberId);
 
-        if(categoryWeights.isEmpty()){
+        if (categoryWeights.isEmpty()) {
             throw new IllegalArgumentException("No Member Weight found with memberId: " + memberId);
         }
 
-        categoryWeights.parallelStream()
-                .peek(CategoryWeight::calculateMemberWeight)
+        categoryWeights.stream()
+                .peek(categoryWeight -> categoryWeight.calculateMemberWeight(now))
                 .filter(CategoryWeight::isExpiredCategoryWeight)
                 .forEach(categoryWeightRepository::delete);
     }
