39cc0958152110b1783905131a0e5ba42c48bf9e
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:31:09 +0900

    refactor: 생성자 접근 제어자 추가

--- drrr-api/src/main/java/com/drrr/recommand/dto/RecommendResponse.java
+++ drrr-api/src/main/java/com/drrr/recommand/dto/RecommendResponse.java
@@ -1,11 +1,17 @@
 package com.drrr.recommand.dto;
 
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Getter
 public class RecommendResponse {
     private List<TechBlogPost> posts;
