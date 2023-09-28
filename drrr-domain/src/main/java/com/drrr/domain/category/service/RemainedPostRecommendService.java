35996f3a37fdf1f22907f03d14ace822beac8196
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:35:38 +0900

    feat: @Transactional 어노테이션 추가

--- drrr-domain/src/main/java/com/drrr/domain/category/service/RemainedPostRecommendService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/RemainedPostRecommendService.java
@@ -9,9 +9,11 @@ import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class RemainedPostRecommendService {
     private JPAQueryFactory queryFactory;
     @PersistenceContext
