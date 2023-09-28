d216ed4d992dd6d120ad65f17151dce9b1f4debd
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:33:24 +0900

    feat: 생성자 접근 제어자 추가

--- drrr-domain/src/main/java/com/drrr/domain/category/entity/Category.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/entity/Category.java
@@ -3,19 +3,17 @@ package com.drrr.domain.category.entity;
 
 import com.drrr.domain.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
 @Entity
