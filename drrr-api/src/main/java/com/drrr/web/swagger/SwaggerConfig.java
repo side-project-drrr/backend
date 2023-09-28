d66464b2ff8fd752d743bb760ef0cb859b59af27
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:32:19 +0900

    feat: swagger 문서에서 json 필드가 SNAKE_STRATEGY로 보이게 설정 변경

--- drrr-api/src/main/java/com/drrr/web/swagger/SwaggerConfig.java
+++ drrr-api/src/main/java/com/drrr/web/swagger/SwaggerConfig.java
@@ -1,24 +1,38 @@
 package com.drrr.web.swagger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverter;
+import io.swagger.v3.core.converter.ModelConverterContext;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("JWT",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info().title("Dr--r--r-- Project Springdoc")
+                        .description("Dr--r--r-- Project Swagger UI")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
     }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("Dr--r--r-- Project Springdoc")
-                .description("Dr--r--r-- Project Swagger UI")
-                .version("1.0.0");
+    //Swagger 문서에서 Example Value를 보여줄 때 SNAKE_CASE로 보여주게 함
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE));
     }
 }
