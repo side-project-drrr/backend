7b2bb6f3fff0d7290ca874098bd250db3373d493
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:25:50 +0900

    feat: swagger 문서 작성 및 validation 추가

--- drrr-api/src/main/java/com/drrr/auth/payload/request/SignUpRequest.java
+++ drrr-api/src/main/java/com/drrr/auth/payload/request/SignUpRequest.java
@@ -4,6 +4,12 @@ package com.drrr.auth.payload.request;
 import com.drrr.core.code.Gender;
 import com.drrr.domain.category.entity.Category;
 import com.drrr.domain.member.service.RegisterMemberService.RegisterMemberDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,19 +23,35 @@ import lombok.NonNull;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class SignUpRequest {
-    @NonNull
+    @Schema(description = "access token", nullable = false, example = "[access token]")
+    @NotNull
     private String accessToken;
-    @NonNull
+    @Schema(description = "사용자 이메일", nullable = false, example = "xxxx@xxxx.com")
+    @Email(message = "알맞은 이메일 형식이 아닙니다")
+    @NotNull
     private String email;
 
-    private List<Long> categoryIds;
+    @Schema(description = "카테고리 id", nullable = false, example = "[1, 2, 3]")
+    private List<@NotNull(message ="사용자의 선호 카테고리id를 지정해주세요")Long> categoryIds;
 
+    @Schema(description = "사용자 별명", nullable = false, example = "홍길동")
+    @NotNull
     private String nickname;
+    @Schema(description = "사용자 전화번호", nullable = true, example = "010xxxxxxxx")
+    @Pattern(regexp="^[0-9]*$", message ="전화번호는 숫자만 입력가능합니다")
     private String phoneNumber;
+    @Schema(description = "성별", nullable = false, example = "MAN or WOMAN")
     private Gender gender;
+    @Schema(description = "생일", nullable = false, example = "1900412")
+    @Pattern(regexp="^\\d{8}$", message = "생일은 YYYYMMDD 형태로 이루어져야 합니다")
     private String birthYear;
+    @Schema(description = "소셜 로그인 주최", nullable = true, example = "kakao")
+    @Pattern(regexp="^[a-zA-Z]*$", message = "provider는 영문자로만 이루어져야 합니다")
     private String provider;
+    @Schema(description = "소셜 로그인 id", nullable = true, example = "12343")
+    @Pattern(regexp="^[0-9]*$",message = "providerId는 숫자만 입력가능합니다")
     private String providerId;
+    @Schema(description = "CDN 이미지 url", nullable = false, example = "https://~")
     private String imageUrl;
 
     public RegisterMemberDto toRegisterMemberDto() {
