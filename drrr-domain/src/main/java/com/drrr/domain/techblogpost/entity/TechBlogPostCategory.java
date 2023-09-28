00b24766835a93ddc4e0b70e60d5d9fd4373a7cf
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:43:48 +0900

    refactor: 코드 리뷰 리펙토링 적용

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPostCategory.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPostCategory.java
@@ -1,40 +1,55 @@
 package com.drrr.domain.techblogpost.entity;
 
 import com.drrr.domain.category.entity.Category;
+import com.drrr.domain.category.service.RecommendPostService.ExtractedPostCategoryDto;
 import com.drrr.domain.jpa.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "DRRR_TECHBLOGPOST_CATEGORY")
 @PrimaryKeyJoinColumn(name = "TECHBLOGPOST_CATEGORY_ID")
 public class TechBlogPostCategory extends BaseEntity {
 
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "techblogpost_id")
     private TechBlogPost post;
 
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
+
+
+    /**
+     * 하나의 게시물에 대한 여러개의 카테고리를 객체로 각각 저장 techBlogPosts
+     */
+    public static Map<Long, Set<Long>> classifyPostWithCategoriesByMap(List<ExtractedPostCategoryDto> techBlogPosts) {
+
+        Map<Long, Set<Long>> classifiedPostsMap = techBlogPosts.stream()
+                .collect(Collectors.groupingBy(
+                        ExtractedPostCategoryDto::postId,
+                        LinkedHashMap::new,
+                        Collectors.mapping(ExtractedPostCategoryDto::categoryId, Collectors.toSet())
+                ));
+        return classifiedPostsMap;
+
+    }
 }
