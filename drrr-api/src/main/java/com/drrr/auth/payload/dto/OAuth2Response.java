3825ba93a9f8f433f752f1d94c2c734ee35dab5e
YoungKyonYou <man.spride1324@gmail.com>
2023-09-28 10:24:55 +0900

    refactor: 생성자 접근제어 추가

--- drrr-api/src/main/java/com/drrr/auth/payload/dto/OAuth2Response.java
+++ drrr-api/src/main/java/com/drrr/auth/payload/dto/OAuth2Response.java
@@ -1,11 +1,12 @@
 package com.drrr.auth.payload.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OAuth2Response {
     private String id;
