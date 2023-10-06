7fa7de55d22ce60f966f3d92ffda7f5860fc6963
YoungKyonYou <man.spride1324@gmail.com>
2023-10-06 16:30:00 +0900

    feat: service 단에 있던 쿼리를 커스텀 repository로 분리하는 작업 수행

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostCategoryRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostCategoryRepository.java
@@ -1,10 +1,12 @@
 package com.drrr.domain.techblogpost.repository;
 
 import com.drrr.domain.techblogpost.entity.TechBlogPostCategory;
+import com.drrr.domain.techblogpost.repository.custom.CustomTechBlogPostCategoryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 
-public interface TechBlogPostCategoryRepository extends JpaRepository<TechBlogPostCategory, Long> {
+@Repository
+public interface TechBlogPostCategoryRepository extends JpaRepository<TechBlogPostCategory, Long>,
+        CustomTechBlogPostCategoryRepository {
 }
