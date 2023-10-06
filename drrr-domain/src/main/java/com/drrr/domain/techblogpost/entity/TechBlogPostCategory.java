6b9313fefb93a350123f1d07b793e7b7be9505fb
YoungKyonYou <man.spride1324@gmail.com>
2023-10-06 16:29:29 +0900

    reformat: 도메인 분리를 위해 제거

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPostCategory.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPostCategory.java
@@ -1,7 +1,6 @@
 package com.drrr.domain.techblogpost.entity;
 
 import com.drrr.domain.category.entity.Category;
-import com.drrr.domain.category.service.RecommendPostService.ExtractedPostCategoryDto;
 import com.drrr.domain.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,11 +8,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,19 +31,4 @@ public class TechBlogPostCategory extends BaseEntity {
     @JoinColumn(name = "category_id")
     private Category category;
 
-
-    /**
-     * 하나의 게시물에 대한 여러개의 카테고리를 객체로 각각 저장 techBlogPosts
-     */
-    public static Map<Long, Set<Long>> classifyPostWithCategoriesByMap(List<ExtractedPostCategoryDto> techBlogPosts) {
-
-        Map<Long, Set<Long>> classifiedPostsMap = techBlogPosts.stream()
-                .collect(Collectors.groupingBy(
-                        ExtractedPostCategoryDto::postId,
-                        LinkedHashMap::new,
-                        Collectors.mapping(ExtractedPostCategoryDto::categoryId, Collectors.toSet())
-                ));
-        return classifiedPostsMap;
-
-    }
 }
