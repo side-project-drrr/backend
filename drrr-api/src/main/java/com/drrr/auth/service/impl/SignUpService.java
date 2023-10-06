30eab7bf233205e24fb870915572e16b0a8b5786
YoungKyonYou <man.spride1324@gmail.com>
2023-10-06 16:21:58 +0900

    feat: 회원가입 서비스에 @Transactional 추가

--- drrr-api/src/main/java/com/drrr/auth/service/impl/SignUpService.java
+++ drrr-api/src/main/java/com/drrr/auth/service/impl/SignUpService.java
@@ -3,14 +3,15 @@ package com.drrr.auth.service.impl;
 
 import com.drrr.auth.payload.request.SignUpRequest;
 import com.drrr.auth.payload.response.SignUpResponse;
-
 import com.drrr.auth.service.impl.IssuanceTokenService.IssuanceTokenDto;
 import com.drrr.domain.category.service.InitializeWeightService;
 import com.drrr.domain.member.service.RegisterMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SignUpService {
     private final ExternalAuthenticationFacade externalAuthenticationFacade;
@@ -20,10 +21,10 @@ public class SignUpService {
     private final InitializeWeightService initializeWeightService;
 
     public SignUpResponse execute(SignUpRequest signUpRequest) {
-        String socialId = externalAuthenticationFacade.execute(signUpRequest.getAccessToken(), signUpRequest.getProvider());
+        String socialId = externalAuthenticationFacade.execute(signUpRequest.accessToken(), signUpRequest.provider());
         Long memberId = registerMemberService.execute(socialId, signUpRequest.toRegisterMemberDto());
         IssuanceTokenDto issuanceTokenDto = issuanceTokenService.execute(memberId);
-        initializeWeightService.initializeCategoryWeight(memberId, signUpRequest.getCategoryIds());
+        initializeWeightService.initializeCategoryWeight(memberId, signUpRequest.categoryIds());
 
         return SignUpResponse.builder()
                 .accessToken(issuanceTokenDto.getAccessToken())
