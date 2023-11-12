24f571d7083a5ba510236a2825aaf52eda2b4f00
YoungKyonYou <man.spride1324@gmail.com>
2023-11-12 18:15:05 +0900

    refactor: prometheus log 안나오게 변경

--- drrr-api/src/main/java/com/drrr/web/security/filter/JwtTokenValidationFilter.java
+++ drrr-api/src/main/java/com/drrr/web/security/filter/JwtTokenValidationFilter.java
@@ -7,18 +7,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -29,20 +28,28 @@ public class JwtTokenValidationFilter extends OncePerRequestFilter {
     private static final String TOKEN_PREFIX = "Bearer ";
     private static final String HEADER_AUTHORIZATION = "Authorization";
     private final JwtProvider jwtTokenProvider;
+    private final Set<String> IgnoreUrlsSet = new HashSet<>(List.of("/actuator/prometheus"));
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain)
             throws ServletException, IOException {
-        log.info("-------------------JwtTokenValidationFilter CALL-------------------");
-        log.info("-------------------request URI: " + request.getRequestURI() + "---------------");
-        var token = extractToken(request);
-        if (Objects.isNull(token)) {
-            System.out.println("-----------JWT null-------------------");
+        if (IgnoreUrlsSet.contains(request.getRequestURI())) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        var memberId = jwtTokenProvider.extractToValueFrom(token);
+        log.info("-------------------JwtTokenValidationFilter CALL-------------------");
+        log.info("-------------------request URI: " + request.getRequestURI() + "---------------");
+        String token = extractToken(request);
+
+        if (Objects.isNull(token) || IgnoreUrlsSet.contains(request.getRequestURI())) {
+            System.out.println("-----------JWT Token null-------------------");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Long memberId = jwtTokenProvider.extractToValueFrom(token);
 
         try {
             if (!jwtTokenProvider.validateToken(token)) {
@@ -66,7 +73,7 @@ public class JwtTokenValidationFilter extends OncePerRequestFilter {
         }
     }
 
-    private String extractToken(HttpServletRequest request) {
+    private String extractToken(final HttpServletRequest request) {
         String token = request.getHeader(HEADER_AUTHORIZATION);
 
         if (token != null && token.startsWith(TOKEN_PREFIX)) {
