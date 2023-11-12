2784244512fe9077b0da03f6166a1cb04ebd75af
YoungKyonYou <man.spride1324@gmail.com>
2023-11-12 17:13:07 +0900

    feat: 소셜 로그인 주체 서버와의 통신하기 위한 restclient 추가

--- drrr-api/src/main/java/com/drrr/auth/infrastructure/authentication/OAuth2Client.java
+++ drrr-api/src/main/java/com/drrr/auth/infrastructure/authentication/OAuth2Client.java
@@ -1,17 +1,19 @@
 package com.drrr.auth.infrastructure.authentication;
 
 
-
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-import com.drrr.auth.payload.dto.OAuth2Response;
+import com.drrr.auth.payload.dto.OAuth2GithubAccessTokenRequest;
+import com.drrr.auth.payload.dto.OAuth2KakaoAccessTokenRequest;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,33 +22,107 @@ public class OAuth2Client {
     private final RestClient restClient = RestClient.create();
     private final ObjectMapper objectMapper;
 
-    public OAuth2Response getUserProfile(final String accessToken, String uri) {
-        OAuth2Response result = restClient.get()
+    public String getUserProfile(final String accessToken, final String uri) {
+        final String providerId = restClient.get()
                 .uri(uri)
                 .header("Authorization", accessToken)
                 .accept(APPLICATION_JSON)
                 .exchange((request, response) -> {
                     if (response.getStatusCode().is4xxClientError()) {
-                        throw new IllegalArgumentException("유효하지 않는 인증" + response.getBody());
+                        throw new IllegalArgumentException(
+                                "Access Token이 유효하지  않습니다. request uri -> " + uri + ", response -> "
+                                        + response.getBody());
+                    } else {
+                        final JsonNode jsonNode = objectMapper.readTree(response.getBody());
+                        //asText()에서 code가 잘못 됐을 경우 null 반환
+                        try {
+                            final String id = jsonNode.get("id").asText();
+                            return id;
+                        } catch (NullPointerException npe) {
+                            log.error("provider ID를 받아오지 못했습니다. Access Token를 확인해주세요.");
+                            throw new RuntimeException("provider ID를 받아오지 못했습니다. Access Token를 확인해주세요.");
+                        }
                     }
-                    else {
-                        return convertResponse(response);
+                });
+        return providerId;
+    }
+
+    /**
+     * Code로 AccessToken 받기
+     */
+    public String exchangeKakaoOAuth2AccessToken(final OAuth2KakaoAccessTokenRequest requestParams) {
+        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(requestParams.uri())
+                .queryParam("grant_type", "authorization_code")
+                .queryParam("client_id", requestParams.clientId())
+                .queryParam("code", requestParams.code());
+
+        final String url = uriBuilder.build().encode().toUriString();
+
+        final String result = restClient.post()
+                .uri(url)
+                .accept(APPLICATION_JSON)
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+                .exchange((request, response) -> {
+                    if (response.getStatusCode().is4xxClientError()) {
+                        log.error(
+                                "OAuth2Client Class exchangeKakaoOAuth2AccessToken(final OAuth2KakaoAccessTokenRequest requestParams) Method IllegalArgumentException Error");
+                        throw new IllegalArgumentException(
+                                "Kakao OAuth2의 인증코드가 유효하지 않습니다. uri -> " + requestParams.uri() + ", response body -> "
+                                        + response.getBody());
+                    } else {
+
+                        final JsonNode jsonNode = objectMapper.readTree(response.getBody());
+                        //asText()에서 code가 잘못 됐을 경우 null 반환
+                        try {
+                            final String accessToken = jsonNode.get("access_token").asText();
+                            return accessToken;
+                        } catch (NullPointerException npe) {
+                            log.error(
+                                    "OAuth2Client Class exchangeKakaoOAuth2AccessToken(final OAuth2KakaoAccessTokenRequest requestParams) Method NullPointerException Error");
+                            throw new RuntimeException("Kakao 인증코드를 확인해주세요");
+                        }
+                    }
+                });
+
+        return result;
+
+    }
+
+    public String exchangeGitHubOAuth2AccessToken(final OAuth2GithubAccessTokenRequest requestBody) {
+        final Map<String, String> body = new HashMap<>();
+        body.put("client_id", requestBody.getClientId());
+        body.put("client_secret", requestBody.getClientSecret());
+        body.put("code", requestBody.getCode());
+
+        final String result = restClient.post()
+                .uri(requestBody.getUri())
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .body(body)
+                .exchange((request, response) -> {
+                    if (response.getStatusCode().is4xxClientError()) {
+                        log.error(
+                                "OAuth2Client Class exchangeGitHubOAuth2AccessToken(OAuth2GithubAccessTokenRequest requestBody) Method IllegalArgumentException Error");
+                        throw new IllegalArgumentException(
+                                "Github OAuth2의 인증코드가 유효하지 않습니다 -> uri ->" + requestBody.getUri()
+                                        + ", response body -> " + response.getBody());
+                    } else {
+                        final JsonNode jsonNode = objectMapper.readTree(response.getBody());
+
+                        //asText()에서 code가 잘못 됐을 경우 null 반환
+                        try {
+                            final String accessToken = jsonNode.get("access_token").asText();
+                            return accessToken;
+                        } catch (NullPointerException npe) {
+                            log.error(
+                                    "OAuth2Client Class exchangeGitHubOAuth2AccessToken(OAuth2GithubAccessTokenRequest requestBody) Method NullPointerException Error");
+                            throw new RuntimeException("Github 인증코드를 확인해주세요");
+                        }
                     }
                 });
 
         return result;
     }
 
-    private OAuth2Response convertResponse(ClientHttpResponse response){
-        OAuth2Response result = null;
-        try {
-            result = objectMapper.readValue(response.getBody(), OAuth2Response.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return result;
-
-    }
 
 }
