4eae9abd6827c79d693ac12094ca6d7e7d6b7d67
YoungKyonYou <man.spride1324@gmail.com>
2023-11-12 17:12:06 +0900

    feat: 소셜 로그인 컨트롤러 수정 및 추가

--- drrr-api/src/main/java/com/drrr/auth/controller/AuthController.java
+++ drrr-api/src/main/java/com/drrr/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.drrr.auth.controller;
 
+import com.drrr.auth.payload.dto.OAuth2Response;
 import com.drrr.auth.payload.request.AccessTokenRequest;
 import com.drrr.auth.payload.request.SignInRequest;
 import com.drrr.auth.payload.request.SignUpRequest;
 import com.drrr.auth.payload.response.AccessTokenResponse;
 import com.drrr.auth.payload.response.SignInResponse;
 import com.drrr.auth.payload.response.SignUpResponse;
+import com.drrr.auth.service.impl.ExchangeOAuth2AccessTokenService;
 import com.drrr.auth.service.impl.IssuanceTokenService;
 import com.drrr.auth.service.impl.SignInService;
 import com.drrr.auth.service.impl.SignUpService;
@@ -15,16 +17,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -32,14 +37,25 @@ public class AuthController {
     private final SignUpService signUpService;
     private final SignInService signInService;
     private final IssuanceTokenService issuanceTokenService;
+    private final ExchangeOAuth2AccessTokenService exchangeOAuth2AccessTokenService;
 
+
+    @Operation(summary = "Front에서 준 code로 provider ID를 반환하는 API", description = "호출 성공 시 provider id와 isRegistered 반환. isRegistered : false (신규회원) true (기존회원)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = SignUpResponse.class)))
+    })
+    @GetMapping("/oauth2/profile")
+    public OAuth2Response exchangeOAuth2AccessToken(@RequestParam("code") final String code,
+                                                    @RequestParam("state") final String provider) {
+        return exchangeOAuth2AccessTokenService.execute(code, provider);
+    }
 
     @Operation(summary = "소셜 로그인 회원가입 호출 API", description = "호출 성공 시 JWT Access, Refresh 토큰 반환")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = SignUpResponse.class)))
     })
     @PostMapping("/signup")
-    public SignUpResponse signUp(@Validated @RequestBody SignUpRequest signUpRequest) {
+    public SignUpResponse signUp(@Validated @RequestBody final SignUpRequest signUpRequest) {
         return signUpService.execute(signUpRequest);
     }
 
@@ -48,7 +64,7 @@ public class AuthController {
             @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = SignInResponse.class)))
     })
     @PostMapping("/signin")
-    public SignInResponse signIn(@Validated @RequestBody SignInRequest signInRequest) {
+    public SignInResponse signIn(@Validated @RequestBody final SignInRequest signInRequest) {
         return signInService.execute(signInRequest);
     }
 
@@ -58,8 +74,16 @@ public class AuthController {
     })
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping("/access-token")
-    public ResponseEntity<AccessTokenResponse> regenerateAccessToken(@Validated @RequestBody final AccessTokenRequest request) {
+    public ResponseEntity<AccessTokenResponse> regenerateAccessToken(
+            @Validated @RequestBody final AccessTokenRequest request) {
         AccessTokenResponse accessTokenResponse = issuanceTokenService.regenerateAccessToken(request);
         return ResponseEntity.ok(accessTokenResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) {
+        String errorMessage = e.getMessage(); // 원하는 에러 메시지를 작성합니다.
+        e.printStackTrace();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
