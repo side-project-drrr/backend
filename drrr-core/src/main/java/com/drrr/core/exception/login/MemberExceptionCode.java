a8e51cc455018fbefda5e8ac90146826ad996bff
YoungKyonYou <man.spride1324@gmail.com>
2023-11-12 21:19:44 +0900

    fix: MemberException 오타 수정

--- drrr-core/src/main/java/com/drrr/core/exception/login/MemberExceptionCode.java
+++ drrr-core/src/main/java/com/drrr/core/exception/login/MemberExceptionCode.java
@@ -14,7 +14,7 @@ public enum MemberExceptionCode {
     INVALID_AUTHORIZE_CODE(MEMBER.code+2, "OAuth2 Authorize Token이 유효하지 않습니다."),
     INVALID_OAUTH2_SUBJECT(MEMBER.code+3, "OAuth2 주체를 찾을 수 없습니다."),
     DUPLICATE_EMAIL(MEMBER.code+4, "이메일이 이미 등록되어 있습니다."),
-    DUPLICATE_NICKNAME(MEMBER.code+5, "닉네임 이미 등록되어 있습니다."),
+    DUPLICATE_NICKNAME(MEMBER.code+5, "닉네임인 이미 등록되어 있습니다."),
     UNKNOWN_ERROR(MEMBER.code+6, "알 수 없는 오류가 발생했습니다."),
     UNREGISTERED_MEMBER(MEMBER.code+7, "등록되지 않은 사용자입니다."),
     ;
