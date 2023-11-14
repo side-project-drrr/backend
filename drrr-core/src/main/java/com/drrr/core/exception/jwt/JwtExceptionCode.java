457fdda222cae129645fef288d4499b53fb3a82c
YoungKyonYou <man.spride1324@gmail.com>
2023-11-14 22:21:06 +0900

    chore : 에러 코드 오타 수정

--- drrr-core/src/main/java/com/drrr/core/exception/jwt/JwtExceptionCode.java
+++ drrr-core/src/main/java/com/drrr/core/exception/jwt/JwtExceptionCode.java
@@ -9,7 +9,7 @@ public enum JwtExceptionCode {
     JWT(3000, "정의되지 않은 에러입니다."),
     EXTRACT_VALUE_FROM_TOKEN_FAILED(JWT.code + 1,"JWT 토큰으로부터 값을 추출할 수 없습니다."),
     INVALID_JWT_SIGNATURE(JWT.code + 2,"JWT 토큰 시그니처가 유효하지 않습니다."),
-    JWT_TOKEN_EXPIRED(JWT.code + 2,"JWT 토큰 시그니처가 유효하지 않습니다."),
+    JWT_TOKEN_EXPIRED(JWT.code + 3,"JWT 토큰 시그니처가 유효하지 않습니다."),
     ;
 
     private static final String ERROR_FORMAT = "[ERROR %d] %s";
