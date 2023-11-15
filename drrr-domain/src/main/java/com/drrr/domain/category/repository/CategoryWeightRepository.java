dc1bc8bcb63a4bcf60d6dc3ccf87dd968b8e56ec
YoungKyonYou <man.spride1324@gmail.com>
2023-11-15 22:59:37 +0900

    refactor: 필요없는 jpql 제거

--- drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
@@ -3,14 +3,13 @@ package com.drrr.domain.category.repository;
 import com.drrr.domain.category.entity.CategoryWeight;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryWeightRepository extends JpaRepository<CategoryWeight, Long> {
-    @Query("select cw from CategoryWeight cw where cw.member.id =:memberId ")
-    List<CategoryWeight> findCategoryWeightsByMemberId(@Param("memberId") Long memberId);
-    List<CategoryWeight> findCategoryWeightByMemberId(Long memberId);
+
     List<CategoryWeight> findByMemberId(Long memberId);
+
+    void deleteByMemberId(Long memberId);
+
 }
