dd4b3174b482d7bf47be6fe806389b6f5a80f4be
YoungKyonYou <man.spride1324@gmail.com>
2023-11-15 22:53:20 +0900

    refactor: 100~500 번대 에러 코드는 http error code랑 헷가릴 수 있으니까 천번대로 변경

--- drrr-core/src/main/java/com/drrr/core/exception/admin/AdminExceptionCode.java
+++ drrr-core/src/main/java/com/drrr/core/exception/admin/AdminExceptionCode.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum AdminExceptionCode {
-    ADMIN(100, "정의되지 않은 에러입니다."),
+    ADMIN(1000, "정의되지 않은 에러입니다."),
     FAIL_SIGNIN(ADMIN.code + 1, "잘못된 관리자 Id 혹은 비밀번호 입니다.");
 
     private static final String ERROR_FORMAT = "[ERROR %d] %s";
