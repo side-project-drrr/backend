1e4fa7c390691068f36b46f874a98ca01d9f59c8
YoungKyonYou <man.spride1324@gmail.com>
2023-11-15 23:01:41 +0900

    feat: 에러 로그 추가

--- drrr-domain/src/main/java/com/drrr/domain/category/service/InitializeWeightService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/InitializeWeightService.java
@@ -10,29 +10,35 @@ import com.drrr.domain.member.repository.MemberRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class InitializeWeightService {
     private final CategoryWeightRepository categoryWeightRepository;
     private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
 
-    public void initializeCategoryWeight(Long memberId, List<Long> categories) {
+    public void initializeCategoryWeight(final Long memberId, final List<Long> categories) {
         List<Category> categoryList = categoryRepository.findIds(categories);
 
         if (categories.isEmpty()) {
+            log.error("InitializeWeightService Class initializeCategoryWeight(final Long memberId, final List<Long> categories) Method RuntimeException Error");
             throw new RuntimeException(
                     "InitializeWeightService.initializeCategoryWeight() - Cannot find such categories");
         }
 
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException(
-                        "InitializeWeightService.initializeCategoryWeight() - No Member Weight found with memberId: "
-                                + memberId));
+                .orElseThrow(() -> {
+                    log.error("initializeCategoryWeight(final Long memberId, final List<Long> categories) Method NoSuchElementException Error");
+                    return new NoSuchElementException(
+                            "InitializeWeightService.initializeCategoryWeight() - No Member Weight found with memberId: "
+                                    + memberId);
+                });
         List<CategoryWeight> initialCategoryWeight = categoryList.stream()
                 .map(category -> CategoryWeight.builder()
                         .member(member)
