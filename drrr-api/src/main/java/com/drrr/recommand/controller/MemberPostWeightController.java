53ed1a45a19157528fa06bb5173a8f9dad1969a7
YoungKyonYou <man.spride1324@gmail.com>
2023-11-15 22:39:30 +0900

    feat: 사용자가 게시물을 읽을 때 가중치를 계산해주는 API

--- drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
+++ drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
@@ -1,23 +1,18 @@
 package com.drrr.recommand.controller;
 
-import com.drrr.auth.payload.response.SignInResponse;
 import com.drrr.recommand.dto.AdjustPostWeightRequest;
 import com.drrr.recommand.service.impl.ExternalMemberPostReadService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -30,8 +25,12 @@ public class MemberPostWeightController {
     @Operation(summary = "사용자 게시물을 읽을 때 가중치 계산 API", description = "호출 성공 시 가중치 계산 및 로깅")
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping("/read/{memberId}/{postId}")
-    public void MemberPostReadController(@Validated @RequestBody AdjustPostWeightRequest request, @NonNull @PathVariable Long memberId,@NonNull @PathVariable Long postId) {
+    public ResponseEntity<String> MemberPostReadController(
+            @Validated @RequestBody final AdjustPostWeightRequest request,
+            @NonNull @PathVariable(name = "memberId") final Long memberId,
+            @NonNull @PathVariable(name = "postId") final Long postId) {
         memberPostReadService.execute(request, memberId, postId);
+        return ResponseEntity.ok().build();
     }
 
 }
