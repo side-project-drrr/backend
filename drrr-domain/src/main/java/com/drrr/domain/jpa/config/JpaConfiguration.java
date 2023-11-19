517e307f127b76fb36630b5534c5681268dfab6e
YoungKyonYou <man.spride1324@gmail.com>
2023-11-19 16:41:11 +0900

    feat: LocalDateTime timezone 세팅

--- drrr-domain/src/main/java/com/drrr/domain/jpa/config/JpaConfiguration.java
+++ drrr-domain/src/main/java/com/drrr/domain/jpa/config/JpaConfiguration.java
@@ -2,8 +2,13 @@ package com.drrr.domain.jpa.config;
 
 
 import com.drrr.domain.Persistence;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +17,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackageClasses = Persistence.class)
 @EnableJpaRepositories(basePackageClasses = {Persistence.class})
 public class JpaConfiguration {
-
+    @Bean
+    public AuditorAware<LocalDateTime> auditorProvider() {
+        return () -> Optional.of(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+    }
 }
 
 
