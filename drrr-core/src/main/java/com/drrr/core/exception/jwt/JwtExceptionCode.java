64c966f0b3a0c2037e8c6f201ffe5e51a2bc8cd9
YoungKyonYou <man.spride1324@gmail.com>
2023-11-19 16:33:59 +0900

    feat: jwt 에러 코드 추가

--- drrr-core/src/main/java/com/drrr/core/exception/jwt/JwtExceptionCode.java
+++ drrr-core/src/main/java/com/drrr/core/exception/jwt/JwtExceptionCode.java
@@ -7,9 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum JwtExceptionCode {
     JWT(3000, "정의되지 않은 에러입니다."),
-    EXTRACT_VALUE_FROM_TOKEN_FAILED(JWT.code + 1,"JWT 토큰으로부터 값을 추출할 수 없습니다."),
-    INVALID_JWT_SIGNATURE(JWT.code + 2,"JWT 토큰 시그니처가 유효하지 않습니다."),
-    JWT_TOKEN_EXPIRED(JWT.code + 3,"JWT 토큰 시그니처가 유효하지 않습니다."),
+    EXTRACT_VALUE_FROM_TOKEN_FAILED(JWT.code + 1, "JWT 토큰으로부터 값을 추출할 수 없습니다."),
+    INVALID_JWT_SIGNATURE(JWT.code + 2, "JWT 토큰 시그니처가 유효하지 않습니다."),
+    JWT_TOKEN_EXPIRED(JWT.code + 3, "JWT 토큰 시그니처가 유효하지 않습니다."),
+    INVALID_JWT_TOKEN(JWT.code + 4, "유효하지 않은 JWT 토큰."),
     ;
 
     private static final String ERROR_FORMAT = "[ERROR %d] %s";
