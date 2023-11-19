fa6c25236187e19cd1ed89a5e6edbeabe4de5a69
YoungKyonYou <man.spride1324@gmail.com>
2023-11-19 16:26:36 +0900

    chore: swagger 臾몄꽌 蹂댁셿

--- drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
+++ drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
@@ -3,6 +3,9 @@ package com.drrr.recommand.controller;
 import com.drrr.recommand.dto.AdjustPostWeightRequest;
 import com.drrr.recommand.service.impl.ExternalMemberPostReadService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +25,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberPostWeightController {
     private final ExternalMemberPostReadService memberPostReadService;
 
-    @Operation(summary = "사용자 게시물을 읽을 때 가중치 계산 API", description = "호출 성공 시 가중치 계산 및 로깅")
+    @Operation(summary = "사용자가 특정 기술 블로그를 읽으려고 클릭했을 때 요청하는 API", description = "호출 성공 시 조회한 기술 블로그 기준으로 가중치 계산, 로깅, 조회수 증가",
+            parameters = {
+                    @Parameter(name = "memberId", description = "게시물을 읽은 사용자 ID", in = ParameterIn.PATH, schema = @Schema(type = "string")),
+                    @Parameter(name = "postId", description = "게시물 ID", in = ParameterIn.PATH, schema = @Schema(type = "string"))
+            })
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping("/read/{memberId}/{postId}")
     public ResponseEntity<String> MemberPostReadController(
