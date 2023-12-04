0e3d83cfd830ddc2c3a224a01028cdd9729ae4da
YoungKyonYou <man.spride1324@gmail.com>
2023-12-04 23:03:18 +0900

    feat: 생성자 추가

--- drrr-core/src/main/java/com/drrr/core/exception/BaseCustomException.java
+++ drrr-core/src/main/java/com/drrr/core/exception/BaseCustomException.java
@@ -1,5 +1,8 @@
 package com.drrr.core.exception;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,8 +11,23 @@ import lombok.RequiredArgsConstructor;
 public abstract class BaseCustomException extends RuntimeException{
     private final int code;
 
-    public BaseCustomException(int code, String message){
+    private final List<Object> args;
+
+    public BaseCustomException(final int code,final String message, final Throwable ex) {
+        super(message, ex);
+        this.code = code;
+        this.args = new ArrayList<>();
+    }
+
+    public BaseCustomException(final int code, final String message) {
         super(message);
         this.code = code;
+        this.args = new ArrayList<>();
     }
+    public BaseCustomException(final int code, final String message, final Object... args) {
+        super(message);
+        this.code = code;
+        this.args = Arrays.asList(args);
+    }
+
 }
