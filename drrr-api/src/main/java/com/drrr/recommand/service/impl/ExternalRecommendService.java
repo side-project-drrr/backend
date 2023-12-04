b85556e7acd88f3a906856d4613382970354bdf3
YoungKyonYou <man.spride1324@gmail.com>
2023-12-04 22:56:51 +0900

    feat: redis관련해서 게시물 캐싱 기능 추가

--- drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalRecommendService.java
+++ drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalRecommendService.java
@@ -2,9 +2,13 @@ package com.drrr.recommand.service.impl;
 
 import com.drrr.domain.category.service.RecommendPostService;
 import com.drrr.domain.category.service.WeightValidationService;
+import com.drrr.domain.log.service.LogUpdateService;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import com.drrr.domain.techblogpost.service.RedisTechBlogPostService;
+import com.drrr.domain.techblogpost.service.TechBlogPostService;
 import com.drrr.recommand.dto.RecommendResponse;
 import com.drrr.recommand.dto.TechBlogPostDto;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,12 +20,33 @@ public class ExternalRecommendService {
 
     private final RecommendPostService recommendPostService;
     private final WeightValidationService weightValidationService;
+    private final RedisTechBlogPostService redisTechBlogPostService;
+    private final TechBlogPostService techBlogPostService;
+    private final LogUpdateService logUpdateService;
 
     @Transactional
     public RecommendResponse execute(final Long memberId) {
-        //두번 째 파라미터의 now는 가중치 검증 test를 진행하기 위함
+        //사용자 가중치 검증
         weightValidationService.validateWeight(memberId);
-        final List<TechBlogPost> posts = recommendPostService.recommendPosts(memberId);
+
+        //추천 게시물 ids 반환받음
+        final List<Long> postIds = recommendPostService.recommendPosts(memberId);
+
+        //redis에서 조회
+        final List<TechBlogPost> postsInRedis = redisTechBlogPostService.findPostsByIds(postIds);
+        final List<TechBlogPost> noCachedPosts = techBlogPostService.findNoCachedTechBlogPosts(postsInRedis, postIds);
+        final List<TechBlogPost> posts = new ArrayList<>(postsInRedis);
+
+        //캐싱해야 할 포스트가 있다면
+        if(!noCachedPosts.isEmpty()){
+            redisTechBlogPostService.savePostsInRedis(noCachedPosts);
+            //합치기
+            posts.addAll(noCachedPosts);
+        }
+
+        //로그 쌓기
+        logUpdateService.updateMemberPostRecommendLog(memberId, postIds);
+
         return RecommendResponse.builder()
                 .posts(posts.stream()
                         .map(post -> TechBlogPostDto.builder()
