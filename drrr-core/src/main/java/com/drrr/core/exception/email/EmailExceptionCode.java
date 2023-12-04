56fcc11d603bdff877753a0c908d3011a3227f00
YoungKyonYou <man.spride1324@gmail.com>
2023-12-04 23:17:23 +0900

    feat: email 관련 인증 서비스 리포지토리 코드 추가

---
+++ drrr-core/src/main/java/com/drrr/core/exception/email/EmailExceptionCode.java
@@ -0,0 +1,16 @@
+package com.drrr.core.exception.email;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum EmailExceptionCode {
+    EMAIL(6000, "정의되지 않은 에러입니다."),
+    EMAIL_VERIFICATION_INFORMATION_NOT_FOUND(EMAIL.code + 1, "이메일 인증 정보를 찾을 수 없습니다."),
+    EMAIL_VERIFICATION_CODE_EXPIRED(EMAIL.code + 2, "이메일 인증코드가 유효하지 않습니다."),
+    ;
+    private final int code;
+    private final String message;
+
+}
