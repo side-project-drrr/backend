ce74519d098aca224e87f5b87f27eddd71ddbc4a
YoungKyonYou <man.spride1324@gmail.com>
2023-12-04 23:09:04 +0900

    feat: in 절로 select하는 jpql 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostRepository.java
@@ -3,6 +3,7 @@ package com.drrr.domain.techblogpost.repository;
 import com.drrr.core.code.techblog.TechBlogCode;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import jakarta.persistence.LockModeType;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -17,5 +18,7 @@ public interface TechBlogPostRepository extends JpaRepository<TechBlogPost, Long
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select tbp from TechBlogPost tbp where tbp.id =:id")
     Optional<TechBlogPost> findByIdWithPessimisticLock(@Param("id") Long id);
+
+    List<TechBlogPost> findByIdIn(List<Long> postIds);
 
 }
