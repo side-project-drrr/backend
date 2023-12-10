510a7bde9120565c33a01329cc0d949fdda54107
YoungKyonYou <man.spride1324@gmail.com>
2023-12-10 23:45:25 +0900

    feat: infra 모듈에서의 빈 스캔 작동 기능 추가

---
+++ drrr-infra/src/main/java/com/drrr/infra/jpa/config/QueryDSLConfiguration.java
@@ -0,0 +1,19 @@
+package com.drrr.infra.jpa.config;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("infraQueryDSLConfiguration")
+public class QueryDSLConfiguration {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
