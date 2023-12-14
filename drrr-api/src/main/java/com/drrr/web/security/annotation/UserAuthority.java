b8b7924d6f04ab7ccda57701a93cd0ba7278803b
YoungKyonYou <man.spride1324@gmail.com>
2023-12-14 12:42:17 +0900

    feat: 커스텀 어노테이션 추가

---
+++ drrr-api/src/main/java/com/drrr/web/security/annotation/UserAuthority.java
@@ -0,0 +1,13 @@
+package com.drrr.web.security.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Target(ElementType.TYPE) // 클래스 단위로 적용
+@Retention(RetentionPolicy.RUNTIME) // 런타임에도 해당 어노테이션 정보를 유지
+@PreAuthorize("hasAuthority('USER')")//JWT 토큰이 없고 security filter chain 명시가 되어 있는 경우
+public @interface UserAuthority {
+}
