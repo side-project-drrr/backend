aea00a6b72c92a2135b517867a4fe82bffbc2fef
YoungKyonYou <man.spride1324@gmail.com>
2023-12-14 12:35:08 +0900

    refactor: 코드 리펙터 적용

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostCategoryRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/TechBlogPostCategoryRepository.java
@@ -2,11 +2,17 @@ package com.drrr.domain.techblogpost.repository;
 
 import com.drrr.domain.techblogpost.entity.TechBlogPostCategory;
 import com.drrr.domain.techblogpost.repository.custom.CustomTechBlogPostCategoryRepository;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TechBlogPostCategoryRepository extends JpaRepository<TechBlogPostCategory, Long>,
         CustomTechBlogPostCategoryRepository {
+
+    @Query("select tbpc.post.id from TechBlogPostCategory tbpc where tbpc.category.id =:id")
+    List<Long> findPostIdsByCategoryId(@Param("id") Long id);
 }
