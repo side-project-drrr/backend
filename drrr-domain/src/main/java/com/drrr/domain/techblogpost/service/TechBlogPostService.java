59456c8bb08e83c700d26cc0645393b15941a421
YoungKyonYou <man.spride1324@gmail.com>
2023-12-14 12:36:03 +0900

    refactor: exception 관련 가져오는 방법 변경 및 stream api 사용

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/service/TechBlogPostService.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/service/TechBlogPostService.java
@@ -1,11 +1,9 @@
 package com.drrr.domain.techblogpost.service;
 
-import com.drrr.core.exception.techblog.TechBlogException;
 import com.drrr.core.exception.techblog.TechBlogExceptionCode;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
-import java.util.ArrayList;
-import java.util.HashSet;
+import com.drrr.domain.techblogpost.repository.custom.CustomTechBlogPostCategoryRepository;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,31 +18,47 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TechBlogPostService {
     private final TechBlogPostRepository techBlogPostRepository;
+    private final CustomTechBlogPostCategoryRepository customTechBlogPostCategoryRepository;
 
-    public List<TechBlogPost> findNoCachedTechBlogPosts(final List<TechBlogPost> postsInRedis, final List<Long> postIds){
-        //추천해줘야 할 전체 게시물 ids
-        final Set<Long> postIdSet = new HashSet<>(postIds);
-
-        // redis에서 가져온 게시물 id를 제외한 나머지 게시물 ids
-        List<Long> noCachedPostIds = postsInRedis.stream()
-                .map(TechBlogPost::getId)
-                .filter(postId -> !postIdSet.contains(postId))
-                .collect(Collectors.toList());
-
-        // 만약 noCachedPostIds가 비어있다면 postIdSet을 사용
-        if(noCachedPostIds.isEmpty()){
-            return techBlogPostRepository.findByIdIn(noCachedPostIds);
-        }
-
-        return new ArrayList<>();
-    }
-
-    public List<TechBlogPost> findTechBlogPostsByIds(List<Long> postIds){
-        List<TechBlogPost> posts = techBlogPostRepository.findByIdIn(postIds);
-        if(posts.size() == 0){
+    public List<TechBlogPost> findAllPosts() {
+        List<TechBlogPost> posts = techBlogPostRepository.findAll();
+        if (posts.size() == 0) {
             log.error("기술블로그를 찾을 수 없습니다.");
-            throw new TechBlogException(TechBlogExceptionCode.TECH_BLOG_NOT_FOUND.getCode(), TechBlogExceptionCode.TECH_BLOG_NOT_FOUND.getMessage());
+            throw TechBlogExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
         }
         return posts;
     }
+
+    public List<TechBlogPost> findPostsByCategory(Long postId) {
+        List<TechBlogPost> posts = customTechBlogPostCategoryRepository.getPostsByCategory(postId);
+        if (posts.size() == 0) {
+            log.error("기술블로그를 찾을 수 없습니다.");
+            throw TechBlogExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
+        }
+        return posts;
+    }
+
+    public List<TechBlogPost> findNotCachedTechBlogPosts(final List<TechBlogPost> postsInRedis,
+                                                         final List<Long> postIds) {
+        //추천해줘야 할 전체 게시물 ids
+        Set<Long> postsInRedisIds = postsInRedis.stream()
+                .map(TechBlogPost::getId)
+                .collect(Collectors.toSet());
+
+        List<Long> notCachedPostIds = postIds.stream()
+                .filter(id -> !postsInRedisIds.contains(id))
+                .collect(Collectors.toList());
+
+        return techBlogPostRepository.findByIdIn(notCachedPostIds);
+    }
+
+    public List<TechBlogPost> findTechBlogPostsByIds(List<Long> postIds) {
+        List<TechBlogPost> posts = techBlogPostRepository.findByIdIn(postIds);
+        if (posts.size() == 0) {
+            log.error("기술블로그를 찾을 수 없습니다.");
+            throw TechBlogExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
+        }
+        return posts;
+    }
+
 }
