5f61bd941847d38aabbcf7addee5190607c571b7
YoungKyonYou <man.spride1324@gmail.com>
2023-12-17 21:48:44 +0900

    feat: redis에서 가져올 수 있도록 기능 추가

--- drrr-api/src/main/java/com/drrr/category/service/impl/ExternalCategoryService.java
+++ drrr-api/src/main/java/com/drrr/category/service/impl/ExternalCategoryService.java
@@ -18,13 +18,16 @@ public class ExternalCategoryService {
     private final CategoryService categoryService;
     private final RedisCategoryService redisCategoryService;
 
+    /**
+     * 모든 카테고리 정보를 가져옴
+     */
     public List<CategoryDto> execute() {
-        List<Category> redisCategories = redisCategoryService.findAll();
+        final List<Category> redisCategories = redisCategoryService.findAll();
         //최신 카테고리가 업데이트되는 순간 모든 캐시를 날림
         if (redisCategories.isEmpty()) {
-            List<CategoryDto> categories = categoryService.findAllCategories();
+            final List<CategoryDto> categories = categoryService.findAllCategories();
 
-            List<RedisCategory> categoryList = categories.stream()
+            final List<RedisCategory> categoryList = categories.stream()
                     .map(categoryDto -> RedisCategory.builder()
                             .id(categoryDto.id())
                             .name(categoryDto.categoryName())
@@ -40,26 +43,41 @@ public class ExternalCategoryService {
                 .toList();
     }
 
-    public List<CategoryDto> execute(CategoryRequest request) {
-        List<Category> redisCategories = redisCategoryService.findByIds(request.categoryIds());
+    /**
+     * 지정된 카테고리 정보를 가져옴
+     */
+    public List<CategoryDto> execute(final CategoryRequest request) {
+        final List<Category> redisCategories = redisCategoryService.findByIds(request.categoryIds());
         if (redisCategories.size() != request.categoryIds().size()) {
-            Set<Long> requestIds = request.categoryIds().stream().collect(Collectors.toSet());
-            Set<Long> redisCategoryIds = redisCategories.stream().map(redisCategory -> redisCategory.getId())
+            final Set<Long> requestIds = request.categoryIds().stream().collect(Collectors.toSet());
+            final Set<Long> redisCategoryIds = redisCategories.stream().map(redisCategory -> redisCategory.getId())
                     .collect(Collectors.toSet());
+            //redis에 없는 카테고리를 뽑아냄
             requestIds.removeAll(redisCategoryIds);
 
-            List<Long> ids = requestIds.stream().toList();
+            final List<Long> ids = requestIds.stream().toList();
 
-            List<CategoryDto> selectedCategories = categoryService.findSelectedCategories(ids);
+            final List<CategoryDto> selectedCategories = categoryService.findSelectedCategories(ids);
 
-            List<RedisCategory> redisCategoryList = selectedCategories.stream()
+            final List<RedisCategory> redisCategoryList = selectedCategories.stream()
                     .filter(redisCategory -> requestIds.contains(redisCategory.id()))
                     .map(filteredCategory -> RedisCategory.builder()
                             .id(filteredCategory.id())
                             .name(filteredCategory.categoryName())
                             .build())
                     .toList();
+
             redisCategoryService.saveCategories(redisCategoryList);
+            //redis에 기존에 있었던 카테고리 dto로 변환
+            final List<CategoryDto> existingCategories = redisCategoryList.stream()
+                    .map(redisCategory -> CategoryDto.builder()
+                            .id(redisCategory.getId())
+                            .categoryName(redisCategory.getName())
+                            .build())
+                    .toList();
+            selectedCategories.addAll(existingCategories);
+
+            return selectedCategories;
         }
         return categoryService.findSelectedCategories(request.categoryIds());
     }
