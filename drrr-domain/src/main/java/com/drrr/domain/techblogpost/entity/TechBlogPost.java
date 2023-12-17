de78ba4fded8771dffceec1af2c2742fd11453fa
YoungKyonYou <man.spride1324@gmail.com>
2023-12-17 21:50:41 +0900

    feat: 기술블로그 엔티티에 새로운 필드 추가 aiSummary와 like 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPost.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/entity/TechBlogPost.java
@@ -18,11 +18,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "DRRR_TECH_BLOG_POST")
+@Table(name = "DRRR_TECHBLOGPOST")
 @PrimaryKeyJoinColumn(name = "TECH_BLOG_POST_ID")
 public class TechBlogPost extends BaseEntity {
-    @Column(nullable = false)
-    private LocalDate createdDate;
 
     // naver의 경우 author가 없는 경우가 있음
     @Column
@@ -53,21 +51,28 @@ public class TechBlogPost extends BaseEntity {
     @Column(nullable = false)
     private int viewCount = 0;
 
+    @Column(nullable = false)
+    private int postLike = 0;
+    @Column(nullable = false)
+    private LocalDate writtenAt;
 
 
     @Builder
-    public TechBlogPost(LocalDate createdDate, String author, String thumbnailUrl, String title, String summary,
+    public TechBlogPost(LocalDate writtenAt, String author, String thumbnailUrl, String title, String summary,
+                        String aiSummary,
                         String urlSuffix, String url,
-                        TechBlogCode crawlerGroup, int viewCount) {
-        this.createdDate = createdDate;
+                        TechBlogCode crawlerGroup, int viewCount, int like) {
+        this.writtenAt = writtenAt;
         this.author = author;
         this.thumbnailUrl = thumbnailUrl;
         this.title = title;
         this.summary = summary;
+        this.aiSummary = aiSummary;
         this.urlSuffix = urlSuffix;
         this.url = url;
         this.techBlogCode = crawlerGroup;
         this.viewCount = viewCount;
+        this.postLike = like;
     }
 
     public static TechBlogPost from(TemporalTechBlogPost temporalTechBlogEntity) {
@@ -75,14 +80,24 @@ public class TechBlogPost extends BaseEntity {
                 .title(temporalTechBlogEntity.getTitle())
                 .author(temporalTechBlogEntity.getAuthor())
                 .crawlerGroup(temporalTechBlogEntity.getTechBlogCode())
-                .createdDate(temporalTechBlogEntity.getCreatedDate())
+                .writtenAt(temporalTechBlogEntity.getWrittenAt())
                 .url(temporalTechBlogEntity.getUrl())
                 .summary(temporalTechBlogEntity.getSummary())
+                .aiSummary(temporalTechBlogEntity.getAiSummary())
                 .urlSuffix(temporalTechBlogEntity.getUrlSuffix())
                 .build();
     }
-    public void increaseViewCount(){
-        this.viewCount+=1;
+
+    public void increaseViewCount() {
+        this.viewCount += 1;
+    }
+
+    public void increaseLikeCount() {
+        this.postLike += 1;
+    }
+
+    public void decreaseLikeCount() {
+        this.postLike -= 1;
     }
 }
 
