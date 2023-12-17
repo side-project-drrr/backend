d4a98b357aaed24969f3c793c2f745acdb2cd43d
YoungKyonYou <man.spride1324@gmail.com>
2023-12-17 21:48:12 +0900

    feat: 카테고리 반환시 정렬 추가

--- drrr-domain/src/main/java/com/drrr/domain/category/service/CategoryService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.drrr.domain.category.service;
 import com.drrr.core.exception.category.CategoryExceptionCode;
 import com.drrr.domain.category.entity.Category;
 import com.drrr.domain.category.repository.CategoryRepository;
+import java.util.Comparator;
 import java.util.List;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
@@ -26,20 +27,24 @@ public class CategoryService {
                         .id(category.getId())
                         .categoryName(category.getName())
                         .build())
+                .sorted(Comparator.comparing(o -> o.categoryName))
                 .toList();
     }
 
     public List<CategoryDto> findSelectedCategories(List<Long> ids) {
         List<Category> categories = categoryRepository.findByIdIn(ids);
+
         if (categories.isEmpty()) {
             log.error("카테고리가 존재하지 않습니다.");
             throw CategoryExceptionCode.CATEGORY_NOT_FOUND.newInstance();
         }
+
         return categories.stream()
                 .map(category -> CategoryDto.builder()
                         .id(category.getId())
                         .categoryName(category.getName())
                         .build())
+                .sorted(Comparator.comparing(o -> o.categoryName))
                 .toList();
     }
 
