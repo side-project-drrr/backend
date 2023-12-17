b41f84806cf038bfd4e886ce0fef51d0ec44d1db
YoungKyonYou <man.spride1324@gmail.com>
2023-12-17 21:57:24 +0900

    feat: 회원탈퇴 api 적용

---
+++ drrr-api/src/main/java/com/drrr/auth/service/impl/UnregisterService.java
@@ -0,0 +1,17 @@
+package com.drrr.auth.service.impl;
+
+import com.drrr.domain.member.service.UnregisterMemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UnregisterService {
+    private final UnregisterMemberService unregisterMemberService;
+
+    public void unregister(final Long memberId) {
+        unregisterMemberService.unregisterMember(memberId);
+    }
+}
