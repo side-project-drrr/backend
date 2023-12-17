2ecf780c7c31d6b221f20a4d401c63cf9234d0b6
YoungKyonYou <man.spride1324@gmail.com>
2023-12-17 21:56:40 +0900

    feat: 기술블로그 데이터를 가져오는 api에서 redis에서 가져올 수 있으면 가져오게 할 것

--- drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/service/RedisTechBlogPostService.java
+++ drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/service/RedisTechBlogPostService.java
@@ -1,11 +1,12 @@
 package com.drrr.domain.techblogpost.service;
 
+import com.drrr.domain.techblogpost.entity.RedisCategoryTechBlogPost;
 import com.drrr.domain.techblogpost.entity.RedisTechBlogPost;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import com.drrr.domain.techblogpost.repository.RedisCategoryTechBlogPostRepository;
 import com.drrr.domain.techblogpost.repository.RedisTechBlogPostRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,22 +15,39 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class RedisTechBlogPostService {
     private final RedisTechBlogPostRepository redisTechBlogPostRepository;
+    private final RedisCategoryTechBlogPostRepository redisCategoryTechBlogPostRepository;
 
-    public List<TechBlogPost> findPostsByIds(List<Long> postIds){
-        final List<RedisTechBlogPost> redisTechBlogPosts = (List<RedisTechBlogPost>)redisTechBlogPostRepository.findAllById(postIds);
+    public List<TechBlogPost> findPostsByIdsInRedis(final List<Long> postIds) {
+        final List<RedisTechBlogPost> redisTechBlogPosts = (List<RedisTechBlogPost>) redisTechBlogPostRepository.findAll();
         return redisTechBlogPosts.stream()
                 .map((entity) -> entity.getTechBlogPost())
                 .toList();
     }
 
-    public void savePostsInRedis(List<TechBlogPost> posts){
+    public List<TechBlogPost> findPostsByCategoryIdInRedis(final Long categoryId) {
+        final RedisCategoryTechBlogPost redisCategoryTechBlogPosts = redisCategoryTechBlogPostRepository.findById(
+                categoryId).get();
+
+        return redisCategoryTechBlogPosts.getTechBlogPost();
+    }
+
+    public void savePostsInRedis(final List<TechBlogPost> posts) {
         final List<RedisTechBlogPost> redisTechBlogPosts = posts.stream()
-                        .map((entity) -> RedisTechBlogPost.builder()
-                                .id(entity.getId())
-                                .techBlogPost(entity)
-                                .build())
-                        .toList();
+                .map((entity) -> RedisTechBlogPost.builder()
+                        .id(entity.getId())
+                        .techBlogPost(entity)
+                        .build())
+                .toList();
 
         redisTechBlogPostRepository.saveAll(redisTechBlogPosts);
+    }
+
+    public void saveCategoryPostsInRedis(final Long categoryId, final List<TechBlogPost> posts) {
+        final RedisCategoryTechBlogPost redisPosts = RedisCategoryTechBlogPost.builder()
+                .id(categoryId)
+                .techBlogPost(posts)
+                .build();
+
+        redisCategoryTechBlogPostRepository.save(redisPosts);
     }
 }
