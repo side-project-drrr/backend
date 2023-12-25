505d1c8ac96fcf0701f91a9e8ea6fa7d8d3ec8ce
YoungKyonYou <man.spride1324@gmail.com>
2023-12-25 22:27:11 +0900

    chore: 웹푸시 구독 신청 할 시 memberId 값은 안 받게 변경 및 deprecated된 NotNull 제거

--- drrr-infra/src/main/java/com/drrr/infra/push/entity/PushMessage.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/entity/PushMessage.java
@@ -1,12 +1,12 @@
 package com.drrr.infra.push.entity;
 
 import lombok.Builder;
-import org.antlr.v4.runtime.misc.NotNull;
+import lombok.NonNull;
 
 @Builder
 public record PushMessage(
-        @NotNull String to,
-        @NotNull String subject,
-        @NotNull String body
+        @NonNull String to,
+        @NonNull String subject,
+        @NonNull String body
 ) {
 }
