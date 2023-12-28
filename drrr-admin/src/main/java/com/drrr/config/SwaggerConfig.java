5c4725d1d1a49344345ab81d9d03eefe5eff9d7b
JaehongDev <println@kakao.com>
2023-12-28 14:47:39 +0900

    feat: swagger, cors 설정

---
+++ drrr-admin/src/main/java/com/drrr/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.drrr.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Dr--r--r-- Project Springdoc")
+                .description("Dr--r--r-- Project Swagger UI")
+                .version("1.0.0");
+    }
+
+}
