ae7bcdcf1379608f72b51381c3256196c1566ece
YoungKyonYou <man.spride1324@gmail.com>
2023-12-30 14:19:52 +0900

    feat: @Auth 어노테이션 추가 및 PathVarable 수정

--- drrr-api/src/main/java/com/drrr/category/controller/CategoryController.java
+++ drrr-api/src/main/java/com/drrr/category/controller/CategoryController.java
@@ -4,6 +4,8 @@ import com.drrr.category.dto.CategoryRequest;
 import com.drrr.category.service.impl.ExternalCategoryService;
 import com.drrr.domain.category.service.CategoryService.CategoryDto;
 import com.drrr.recommand.service.impl.ExternalMemberPreferredCategoryModificationService;
+import com.drrr.web.interceptor.annotation.Auth;
+import com.drrr.web.resolver.annotation.UserId;
 import com.drrr.web.security.annotation.UserAuthority;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -49,24 +51,26 @@ public class CategoryController {
         return externalCategoryService.execute(new CategoryRequest(ids));
     }
 
+
     @Operation(summary = "사용자가 선호카테고리를 바꿀 때 호출하는 API", description = "호출 성공 시 사용자의 선호나는 카테고리 변경 - 올림차순 반환",
             parameters = {
                     @Parameter(name = "memberId", description = "사용자 ID", in = ParameterIn.PATH, schema = @Schema(type = "string")),
                     @Parameter(name = "categoryIds", description = "카테고리 ID 리스트, body 안에 Json 형태로 \"category\" : [1,2,3] 이렇게 body에 넣어줄 것", schema = @Schema(type = "array", implementation = Long.class))
             })
-    @PutMapping("/member/{memberId}/preferences/categories")
-    public ResponseEntity<String> modifyCategory(@NotNull @PathVariable(name = "memberId") final Long memberId,
+    @Auth
+    @PutMapping("/member/preferences/categories")
+    public ResponseEntity<String> modifyCategory(@UserId final Long memberId,
                                                  @RequestBody @NotNull final CategoryRequest request) {
         modificationService.execute(memberId, request.categoryIds());
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "사용자가 가장 많이 선호하는 top 카테고리들을 가져오는 API", description = "호출 성공 시 사용자가 선호하는 카테고리들 반환 - 올림차순 반환",
+    @Operation(summary = "사용자가 가장 많이 선호하는 top 카테고리들을 가져오는 API(가장 선호도가 많은 카테고리순, 내림차순)", description = "호출 성공 시 사용자가 선호하는 카테고리들 반환 - 올림차순 반환",
             parameters = {
                     @Parameter(name = "topN", description = "탑 카테고리 개수", in = ParameterIn.PATH, schema = @Schema(type = "Long"))
             })
-    @GetMapping("/top/categories/{topN}")
-    public List<CategoryDto> findTopCategories(@PathVariable("topN") @NotNull final Long topN) {
+    @GetMapping("/top/categories/{count}")
+    public List<CategoryDto> findTopCategories(@PathVariable("count") @NotNull final Long topN) {
         return externalCategoryService.execute(topN);
     }
 }
