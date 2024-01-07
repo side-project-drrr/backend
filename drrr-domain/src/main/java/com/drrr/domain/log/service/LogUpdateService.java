e62f75c42ddcbff10858ccea2fba16be77cb2a5e
YoungKyonYou <man.spride1324@gmail.com>
2024-01-07 18:34:33 +0900

    chore: 사용 안하는 메서드 제거

--- drrr-domain/src/main/java/com/drrr/domain/log/service/LogUpdateService.java
+++ drrr-domain/src/main/java/com/drrr/domain/log/service/LogUpdateService.java
@@ -1,6 +1,5 @@
 package com.drrr.domain.log.service;
 
-import com.drrr.core.exception.log.LoggingExceptionCode;
 import com.drrr.domain.log.entity.history.MemberPostHistory;
 import com.drrr.domain.log.entity.post.MemberPostLog;
 import com.drrr.domain.log.repository.MemberPostHistoryRepository;
@@ -33,22 +32,6 @@ public class LogUpdateService {
                         .lastReadAt(LocalDateTime.now())
                         .isRecommended(false)
                         .build()));
-    }
-
-    public void updateMemberPostRecommendLog(final Long memberId, final List<Long> postIds) {
-        //해당 유저의 추천받은 기술블로그 ids와 member id로 log 정보 가져오기
-        final List<MemberPostLog> logs = memberPostLogRepository.updateMemberPostLog(memberId, postIds);
-
-        //추천 받은적이 없고 읽었던 기술블로그가 아니여야함
-        //getFilteredPost 메서드에서 log 테이블에 존재하지 않는 posts를 추천하기 때문
-        if (!logs.isEmpty()) {
-            log.error("기술 블로그 추천 후 로깅이 제대로 동작하지 않습니다.");
-            log.error("memberId -> {} ", memberId);
-            throw LoggingExceptionCode.INVALID_RECOMMEND_POSTS_LOGGING.newInstance();
-        }
-
-        //로그 데이터 INSERT
-        insertMemberPostLog(memberId, postIds);
     }
 
     public void insertMemberPostLog(final Long memberId, final List<Long> postIds) {
