b41e9d537f8699ad50d168697e00e736a0ceddc7
YoungKyonYou <man.spride1324@gmail.com>
2024-01-07 21:51:20 +0900

    refactor: 비동기로 변경

--- drrr-api/src/main/java/com/drrr/auth/service/impl/UnregisterService.java
+++ drrr-api/src/main/java/com/drrr/auth/service/impl/UnregisterService.java
@@ -1,6 +1,8 @@
 package com.drrr.auth.service.impl;
 
 import com.drrr.domain.member.service.UnregisterMemberService;
+import com.drrr.infra.push.service.SubscriptionService;
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,8 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UnregisterService {
     private final UnregisterMemberService unregisterMemberService;
+    private final SubscriptionService subscriptionService;
 
     public void execute(final Long memberId) {
-        unregisterMemberService.unregisterMember(memberId);
+        //CompletableFuture
+        CompletableFuture.runAsync(() -> unregisterMemberService.unregisterMember(memberId))
+                .thenRun(() -> subscriptionService.deleteMemberSubscriptionData(memberId))
+                .join();
     }
 }
