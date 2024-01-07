c28122a20c7e7632a0451bc5f7fca0dda7ca6095
YoungKyonYou <man.spride1324@gmail.com>
2024-01-07 18:03:41 +0900

    feat: 사용자게시물로그 테이블에 lastReadAt 필드 추가(마지막으로 게시물을 읽은 시간 기록)

--- drrr-domain/src/main/java/com/drrr/domain/log/service/LogUpdateService.java
+++ drrr-domain/src/main/java/com/drrr/domain/log/service/LogUpdateService.java
@@ -1,14 +1,11 @@
 package com.drrr.domain.log.service;
 
-import static com.drrr.domain.log.entity.post.QMemberPostLog.memberPostLog;
-
 import com.drrr.core.exception.log.LoggingExceptionCode;
 import com.drrr.domain.log.entity.history.MemberPostHistory;
 import com.drrr.domain.log.entity.post.MemberPostLog;
 import com.drrr.domain.log.repository.MemberPostHistoryRepository;
 import com.drrr.domain.log.repository.MemberPostLogRepository;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,34 +19,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Transactional
 public class LogUpdateService {
-    private final JPAQueryFactory queryFactory;
     private final MemberPostHistoryRepository memberPostHistoryRepository;
     private final MemberPostLogRepository memberPostLogRepository;
 
     public void insertMemberPostReadLog(final Long memberId, final Long postId) {
-        memberPostLogRepository.findByPostIdAndMemberId(memberId, postId)
-                .orElseGet(() -> memberPostLogRepository.save(MemberPostLog.builder()
+        //기존에 로그가 있어도 마지막 게시물을 읽은 시간을 업데이트 해야함(새벽 12시 기준으로 다시 읽으면 업데이트를 안하기 위함-조회수)
+        memberPostLogRepository.findByPostIdAndMemberId(memberId, postId).ifPresentOrElse(
+                existingLog -> existingLog.updateLastReadAt(LocalDateTime.now()),
+                () -> memberPostLogRepository.save(MemberPostLog.builder()
                         .memberId(memberId)
                         .postId(postId)
                         .isRead(true)
+                        .lastReadAt(LocalDateTime.now())
                         .isRecommended(false)
                         .build()));
     }
 
     public void updateMemberPostRecommendLog(final Long memberId, final List<Long> postIds) {
         //해당 유저의 추천받은 기술블로그 ids와 member id로 log 정보 가져오기
-        final List<MemberPostLog> logs = queryFactory
-                .selectFrom(memberPostLog)
-                .where(postIdsInOrEq(postIds), memberPostLog.memberId.eq(memberId))
-                .fetch();
+        final List<MemberPostLog> logs = memberPostLogRepository.updateMemberPostLog(memberId, postIds);
 
         //추천 받은적이 없고 읽었던 기술블로그가 아니여야함
         //getFilteredPost 메서드에서 log 테이블에 존재하지 않는 posts를 추천하기 때문
         if (!logs.isEmpty()) {
             log.error("기술 블로그 추천 후 로깅이 제대로 동작하지 않습니다.");
-            log.error("memberId -> " + memberId);
+            log.error("memberId -> {} ", memberId);
             throw LoggingExceptionCode.INVALID_RECOMMEND_POSTS_LOGGING.newInstance();
-
         }
 
         //로그 데이터 INSERT
@@ -81,8 +76,4 @@ public class LogUpdateService {
         memberPostHistoryRepository.save(history);
     }
 
-    private BooleanExpression postIdsInOrEq(final List<Long> postIds) {
-        return postIds.size() == 1 ? memberPostLog.postId.eq(postIds.get(0))
-                : memberPostLog.postId.in(postIds);
-    }
 }
