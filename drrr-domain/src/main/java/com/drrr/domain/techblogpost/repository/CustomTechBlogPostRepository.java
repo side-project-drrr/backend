12689f032796a72893059d2d4e0b3c1b10082b84
YoungKyonYou <man.spride1324@gmail.com>
2024-01-08 22:26:18 +0900

    feat: 카테고리로 게시물을 찾는 query와 탑 게시물 찾는 커스텀 리포지토리 메서드 추가 및 구현 코드 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/CustomTechBlogPostRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/CustomTechBlogPostRepository.java
@@ -1,7 +1,12 @@
 package com.drrr.domain.techblogpost.repository;
 
+import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import java.util.List;
 
 public interface CustomTechBlogPostRepository {
     List<Long> recommendRemain(Long memberId, int remainedPostCount);
+
+    List<TechBlogPost> findPostsByCategory(final Long postId);
+
+    List<TechBlogPost> findTopLikePost(final int count);
 }
--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/impl/CustomTechBlogPostRepositoryImpl.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/repository/impl/CustomTechBlogPostRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.drrr.domain.techblogpost.repository.impl;
 
 import static com.drrr.domain.log.entity.post.QMemberPostLog.memberPostLog;
 import static com.drrr.domain.techblogpost.entity.QTechBlogPost.techBlogPost;
+import static com.drrr.domain.techblogpost.entity.QTechBlogPostCategory.techBlogPostCategory;
 
+import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import com.drrr.domain.techblogpost.repository.CustomTechBlogPostRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -26,6 +28,25 @@ public class CustomTechBlogPostRepositoryImpl implements CustomTechBlogPostRepos
                 .where(memberPostLog.id.isNull())
                 .orderBy(techBlogPost.writtenAt.desc())
                 .limit(remainedPostCount)
+                .fetch();
+    }
+
+    @Override
+    public List<TechBlogPost> findPostsByCategory(Long postId) {
+        return queryFactory.select(techBlogPost)
+                .from(techBlogPostCategory)
+                .leftJoin(techBlogPost)
+                .on(techBlogPostCategory.post.id.eq(techBlogPost.id))
+                .where(techBlogPostCategory.category.id.eq(postId))
+                .fetch();
+    }
+
+    @Override
+    public List<TechBlogPost> findTopLikePost(int count) {
+        return queryFactory.select(techBlogPost)
+                .from(techBlogPost)
+                .orderBy(techBlogPost.postLike.desc(), techBlogPost.writtenAt.desc())
+                .limit(count)
                 .fetch();
     }
 }
