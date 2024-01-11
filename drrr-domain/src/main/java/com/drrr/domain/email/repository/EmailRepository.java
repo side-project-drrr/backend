6319afa0547b7504de38033d2b927aeb579bb482
YoungKyonYou <man.spride1324@gmail.com>
2024-01-11 16:35:35 +0900

    refactor: 이메일 인증 관련 리팩토링

--- drrr-domain/src/main/java/com/drrr/domain/email/repository/EmailRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/email/repository/EmailRepository.java
@@ -1,11 +1,13 @@
 package com.drrr.domain.email.repository;
 
 import com.drrr.domain.email.entity.Email;
-import com.drrr.domain.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
     Optional<Email> findByProviderId(String providerId);
+
     void deleteByProviderId(String providerId);
+
+    boolean existsByProviderId(String providerId);
 }
