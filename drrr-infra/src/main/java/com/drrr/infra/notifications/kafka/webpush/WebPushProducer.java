dc871d59a2aa8f3fff17d2b1d645657d39763b68
YoungKyonYou <man.spride1324@gmail.com>
2024-01-11 13:33:14 +0900

    feat: 천명 단위로 비동기로 푸시 알림을 보내는 코드 작성

--- drrr-infra/src/main/java/com/drrr/infra/notifications/kafka/webpush/WebPushProducer.java
+++ drrr-infra/src/main/java/com/drrr/infra/notifications/kafka/webpush/WebPushProducer.java
@@ -1,14 +1,21 @@
 package com.drrr.infra.notifications.kafka.webpush;
 
 
+import com.drrr.domain.category.repository.CategoryRepository;
+import com.drrr.domain.category.repository.CategoryWeightRepository;
 import com.drrr.infra.notifications.kafka.webpush.dto.NotificationDto;
 import com.drrr.infra.push.entity.Subscription;
 import com.drrr.infra.push.repository.SubscriptionRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @Slf4j
@@ -16,24 +23,30 @@ import org.springframework.stereotype.Service;
 public class WebPushProducer {
     private final SubscriptionRepository subscriptionRepository;
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final CategoryWeightRepository categoryWeightRepository;
+
 
     public void sendNotifications() {
-        final List<Subscription> subscriptions = subscriptionRepository.findAll();
-        if (subscriptions.size() == 0) {
-            log.error("[send Notifiction Failed]");
-            log.error("웹 푸시가 실패하였습니다.");
-            throw new IllegalArgumentException("subscription elements is null");
+        List<Long> memberIds = categoryWeightRepository.findMemberIdsByCategoryWeights();
+
+        int chunkSize = 1000;
+
+        // 천 개씩 끊어서 비동기 작업 생성
+        for (int i = 0; i < memberIds.size(); i += chunkSize) {
+            List<Long> subList = memberIds.subList(i, Math.min(memberIds.size(), i + chunkSize));
+            CompletableFuture.supplyAsync(() ->
+                    subscriptionRepository.findByMemberIdIn(subList)
+            ).thenAccept((subscriptions) -> {
+                subscriptions.forEach(subscription -> {
+                    NotificationDto notification = NotificationDto.builder()
+                            .endPoint(subscription.getEndpoint())
+                            .p245dh(subscription.getP256dh())
+                            .payload("좋아하실만한 기술 블로그를 추천드려요.")
+                            .auth(subscription.getAuth())
+                            .build();
+                    this.kafkaTemplate.send("alarm-web-push", notification);
+                });
+            });
         }
-
-        subscriptions.stream().forEach(subscription -> {
-            NotificationDto notification = NotificationDto.builder()
-                    .endPoint(subscription.getEndpoint())
-                    .p245dh(subscription.getP256dh())
-                    .payload("웹 푸시 내용이 여기에 들어갑니다.")
-                    .auth(subscription.getAuth())
-                    .build();
-            this.kafkaTemplate.send("alarm-web-push", notification);
-        });
-
     }
 }
