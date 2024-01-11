e3593d9b74e155051a946a2eb0058467b209692f
YoungKyonYou <man.spride1324@gmail.com>
2024-01-11 13:32:41 +0900

    feat: 푸시 알림을 보내주기한 memberId를 추출하기 위해서 커스텀 리포지토리 작성

--- drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/repository/CategoryWeightRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryWeightRepository extends JpaRepository<CategoryWeight, Long>{
+public interface CategoryWeightRepository extends JpaRepository<CategoryWeight, Long>, CustomCategoryWeightRepository{
 
     List<CategoryWeight> findByMemberId(Long memberId);
 
