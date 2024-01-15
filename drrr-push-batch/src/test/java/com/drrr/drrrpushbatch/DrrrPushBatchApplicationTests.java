4b0436d779b63b313a39023fb553e57c4dc8401b
YoungKyonYou <man.spride1324@gmail.com>
2024-01-15 20:55:29 +0900

    feat: web push 관련 batch 모듈 추가

---
+++ drrr-push-batch/src/test/java/com/drrr/drrrpushbatch/DrrrPushBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drrr.drrrpushbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrrrPushBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
