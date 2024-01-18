92a0c14b033fb9a39b19b86969e1e5a194fcc9d3
JaehongDev <println@kakao.com>
2024-01-18 14:50:39 +0900

    refactor: 배민 기술블로그 크롤링 방식 변경

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@
 
 date='2023-12-01'
 
-./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=100 requestDate=$date" &
-./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=101 requestDate=$date" &
-./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=102 requestDate=$date" &
-./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=103 requestDate=$date" &
+./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=101 requestDate=$date"
+./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=102 requestDate=$date"
+./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=103 requestDate=$date"
+./gradlew drrr-batch:bootRun --args="--spring.batch.job.name=WebCrawlerBatchJob techBlogCode=104 requestDate=$date"
