17e3494ce00d27073a8ef8cee649eb7025e51f64
YoungKyonYou <man.spride1324@gmail.com>
2024-02-03 20:20:03 +0900

    refactor: e.printstackTrace 제거하고 log로 대체

--- drrr-api/src/main/java/com/drrr/error/GlobalExceptionHandler.java
+++ drrr-api/src/main/java/com/drrr/error/GlobalExceptionHandler.java
@@ -14,29 +14,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(BaseCustomException.class)
     public ResponseEntity<ErrorResponse> handleException(BaseCustomException e) {
-        e.printStackTrace();
+        log.error("{}", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(e.getCode(), e.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> ArgumentException(IllegalArgumentException e) {
-        e.printStackTrace();
+        log.error("{}", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(400, e.getMessage()));
     }
 
     @ExceptionHandler(PropertyReferenceException.class)
     public ResponseEntity<ErrorResponse> PropertyException(PropertyReferenceException e) {
-        e.printStackTrace();
+        log.error("{}", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(400, e.getMessage()));
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorResponse> constraintViolationException(ConstraintViolationException exception) {
+    public ResponseEntity<ErrorResponse> constraintViolationException(ConstraintViolationException e) {
+        log.error("{}", e);
         return ResponseEntity.badRequest()
-                .body(ErrorResponse.of(0, exception.getMessage()));
+                .body(ErrorResponse.of(0, e.getMessage()));
     }
 
 }
