b225b70da779d5caa1bfdbd078e37f6fa6460ca4
YoungKyonYou <man.spride1324@gmail.com>
2024-02-03 22:44:31 +0900

    refactor: sequence 추가

--- drrr-infra/src/main/java/com/drrr/infra/push/entity/PushPost.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/entity/PushPost.java
@@ -2,10 +2,11 @@ package com.drrr.infra.push.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,8 +20,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "DRRR_PUSH_POST")
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@PrimaryKeyJoinColumn(name = "PUSH_POST")
 public class PushPost {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
     private Long postId;
--- drrr-infra/src/main/java/com/drrr/infra/push/entity/PushStatus.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/entity/PushStatus.java
@@ -1,8 +1,9 @@
 package com.drrr.infra.push.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AccessLevel;
@@ -17,8 +18,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "DRRR_PUSH_STATUS")
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@PrimaryKeyJoinColumn(name = "PUSH_STATUS_ID")
 public class PushStatus {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
     private LocalDate pushDate;
