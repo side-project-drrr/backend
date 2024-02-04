f803a21b6e310595caa6ae8419b63e37f70b5992
YoungKyonYou <man.spride1324@gmail.com>
2024-02-05 00:28:24 +0900

    refactor: page로 최적화

--- drrr-domain/src/main/java/com/drrr/domain/category/repository/CustomCategoryWeightRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/repository/CustomCategoryWeightRepository.java
@@ -2,10 +2,10 @@ package com.drrr.domain.category.repository;
 
 
 import com.drrr.domain.category.dto.PushPostDto;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 public interface CustomCategoryWeightRepository {
-    Slice<PushPostDto> findMemberIdsByCategoryWeights(final Pageable pageable);
+    Page<PushPostDto> findMemberIdsByCategoryWeights(final Pageable pageable);
 
 }
