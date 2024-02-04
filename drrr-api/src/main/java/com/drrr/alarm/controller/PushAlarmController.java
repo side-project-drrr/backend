6471c4e6564d0e51f5071178bfa794814444bee9
YoungKyonYou <man.spride1324@gmail.com>
2024-02-04 12:16:48 +0900

    feat: 사용자 푸시 알림 상태 값 바꾸는 api 추가

--- drrr-api/src/main/java/com/drrr/alarm/controller/PushAlarmController.java
+++ drrr-api/src/main/java/com/drrr/alarm/controller/PushAlarmController.java
@@ -1,9 +1,11 @@
 package com.drrr.alarm.controller;
 
+import com.drrr.alarm.dto.PushRequest;
 import com.drrr.alarm.service.impl.ExternalMemberSubscriptionService;
 import com.drrr.alarm.service.impl.ExternalMemberWebPushPostsService;
 import com.drrr.alarm.service.request.SubscriptionRequest;
 import com.drrr.domain.techblogpost.dto.TechBlogPostBasicInfoDto;
+import com.drrr.infra.push.repository.PushStatusRepository;
 import com.drrr.infra.push.repository.SubscriptionRepository;
 import com.drrr.web.jwt.util.JwtProvider;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +37,17 @@ public class PushAlarmController {
 
     private final JwtProvider jwtProvider;
     private final SubscriptionRepository subscriptionRepository;
+    private final PushStatusRepository pushStatusRepository;
+
+    @Operation(summary = "사용자가 푸시 알림 클릭 시 호출 - [JWT TOKEN REQUIRED]",
+            description = "호출 성공 시 사용자의 푸시 알림 상태를 변경 (읽음 처리)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용자의 푸시 알림 상태를 변경", content = @Content(schema = @Schema(implementation = HttpStatus.class)))
+    })
+    @PatchMapping("/push/status/read")
+    public void changeMemberPushStatus(@RequestBody final PushRequest request) {
+        pushStatusRepository.updatePushStatus(request.memberId(), request.pushDate());
+    }
 
     @Operation(summary = "구독 요청 API - [JWT TOKEN REQUIRED]",
             description = "호출 성공 시 사용자의 구독을 요청함")
--- drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
@@ -1,9 +1,18 @@
 package com.drrr.infra.push.repository;
 
 import com.drrr.infra.push.entity.PushStatus;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Repository
 public interface PushStatusRepository extends JpaRepository<PushStatus, Long> {
+    @Modifying
+    @Query("UPDATE PushStatus ps set ps.status = true where ps.memberId = :memberId and ps.pushDate = :pushDate")
+    int updatePushStatus(@Param("memberId") final Long memberId, @Param("pushDate") final LocalDate pushDate);
 }
