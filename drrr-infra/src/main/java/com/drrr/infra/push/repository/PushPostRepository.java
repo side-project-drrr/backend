640644b24f238f6622d8f19b4959e2b8c4174cac
YoungKyonYou <man.spride1324@gmail.com>
2024-02-04 19:16:01 +0900

    feat: 사용자가 push 를 클릭 시 반환해줘야 하는 기술 블로그 정보를 담은 api 추가

--- drrr-infra/src/main/java/com/drrr/infra/push/repository/PushPostRepository.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/repository/PushPostRepository.java
@@ -1,9 +1,22 @@
 package com.drrr.infra.push.repository;
 
 import com.drrr.infra.push.entity.PushPost;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PushPostRepository extends JpaRepository<PushPost, Long> {
+    @Query("""
+                      select pp.postId 
+                        from PushPost pp 
+            right outer join PushStatus ps
+                          on ps.id = pp.pushStatus.id 
+                       where ps.memberId = :memberId AND ps.pushDate = :pushDate 
+                      """)
+    List<Long> findPostIdByMemberIdAndPushDate(@Param("memberId") final Long memberId,
+                                               @Param("pushDate") final LocalDate pushDate);
 }
--- drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @Repository
 public interface PushStatusRepository extends JpaRepository<PushStatus, Long> {
     @Modifying
