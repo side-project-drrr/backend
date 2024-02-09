bd5b8d0e7cdb787b4c25a29dd29a25b5963e37b2
YoungKyonYou <man.spride1324@gmail.com>
2024-02-09 21:06:34 +0900

    feat: Custom Repository 추가 -> 사용자에게 푸시할 게시물 찾기, 로그가 존재하는지 여부

--- drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/repository/PushStatusRepository.java
@@ -2,6 +2,7 @@ package com.drrr.infra.push.repository;
 
 import com.drrr.infra.push.entity.PushStatus;
 import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,8 +10,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PushStatusRepository extends JpaRepository<PushStatus, Long> {
+public interface PushStatusRepository extends JpaRepository<PushStatus, Long>, CustomPushStatusRepository {
     @Modifying
     @Query("update PushStatus ps set ps.status = true where ps.memberId = :memberId and ps.pushDate = :pushDate")
     int updatePushStatus(@Param("memberId") final Long memberId, @Param("pushDate") final LocalDate pushDate);
+
+    List<Long> findPostIdsByMemberIdAndPushDate(final Long memberId, final LocalDate pushDate);
+
+    boolean existsPushStatusByMemberIdAndPushDate(final Long memberId, final LocalDate pushDate);
 }
