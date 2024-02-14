0edc10ff799d2b708304fb305d06cb5142f1a563
YoungKyonYou <man.spride1324@gmail.com>
2024-02-14 21:46:42 +0900

    feat: 게시물 상세보기 반환 필드 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostDetailedInfoDto.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostDetailedInfoDto.java
@@ -2,6 +2,7 @@ package com.drrr.domain.techblogpost.dto;
 
 import com.drrr.core.code.techblog.TechBlogCode;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
@@ -11,15 +12,23 @@ public record TechBlogPostDetailedInfoDto(
         TechBlogCode techBlogCode,
         String thumbnailUrl,
         String aiSummary,
+        LocalDate writtenDate,
+        int viewCount,
+        int postLikeCount,
+        String author,
         String url
 ) {
-    public static TechBlogPostDetailedInfoDto from(final TechBlogPost post){
+    public static TechBlogPostDetailedInfoDto from(final TechBlogPost post) {
         return TechBlogPostDetailedInfoDto.builder()
                 .aiSummary(post.getAiSummary())
                 .techBlogCode(post.getTechBlogCode())
                 .id(post.getId())
                 .thumbnailUrl(post.getThumbnailUrl())
                 .title(post.getTitle())
+                .writtenDate(post.getWrittenAt())
+                .viewCount(post.getViewCount())
+                .postLikeCount(post.getPostLike())
+                .author(post.getAuthor())
                 .url(post.getUrl())
                 .build();
     }
