4bb4a85989b050914617ec6cd20a9c9db14774d3
YoungKyonYou <man.spride1324@gmail.com>
2024-02-17 11:08:26 +0900

    refactor: MemberPostWeightController 스웨거 및 api url 변경

--- drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
+++ drrr-api/src/main/java/com/drrr/recommand/controller/MemberPostWeightController.java
@@ -5,10 +5,14 @@ import com.drrr.web.annotation.MemberId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberPostWeightController {
     private final ExternalMemberPostReadService memberPostReadService;
 
-    @Operation(summary = "사용자가 특정 기술 블로그를 읽으려고 클릭했을 때 요청하는 API - [JWT TOKEN REQUIRED]", description = "호출 성공 시 조회한 기술 블로그 기준으로 가중치 계산, 로깅, 조회수 증가",
+    @Operation(summary = "사용자가 특정 기술 블로그를 읽으려고 클릭했을 때 요청하는 API - [JWT TOKEN REQUIRED]"
+            , description = "호출 성공 시 조회한 기술 블로그 기준으로 가중치 계산, 로깅, 조회수 증가",
             parameters = {
-                    @Parameter(name = "memberId", description = "게시물을 읽은 사용자 ID", in = ParameterIn.PATH, schema = @Schema(type = "string")),
                     @Parameter(name = "postId", description = "게시물 ID", in = ParameterIn.PATH, schema = @Schema(type = "string"))
             })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회한 기술 블로그 기준으로 가중치 계산, 로깅, 조회수 증가"
+                    , content = @Content(schema = @Schema(implementation = HttpStatus.class)))
+    })
     @Secured("USER")
-    @PostMapping("/posts/read/{postId}")
+    @PostMapping("/members/me/read-post/{postId}")
     public void MemberPostReadController(@MemberId final Long memberId,
                                          @NotNull @PathVariable(name = "postId") final Long postId) {
         memberPostReadService.execute(memberId, postId);
