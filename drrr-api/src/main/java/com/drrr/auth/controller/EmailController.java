4b666d225dc5e23b5e9500165020382c82e05af0
YoungKyonYou <man.spride1324@gmail.com>
2024-02-17 11:07:28 +0900

    refactor: emailController 스웨거 및 api url 변경

--- drrr-api/src/main/java/com/drrr/auth/controller/EmailController.java
+++ drrr-api/src/main/java/com/drrr/auth/controller/EmailController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,20 +24,20 @@ public class EmailController {
     private final EmailVerificationService emailVerificationService;
     private final IssuanceVerificationCode issuanceVerificationCode;
 
-    @Operation(summary = "이메일 인증코드 확인 API", description = "호출 성공 시 이메일 인증코드 확인 여부")
+    @Operation(summary = "이메일 인증코드 검증 API", description = "호출 성공 시 이메일 인증코드 검증")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "이메일 인증코드 확인 여부 반환", content = @Content(schema = @Schema(implementation = VerificationDto.class)))
+            @ApiResponse(responseCode = "200", description = "providerId 및 이메일 인증코드 검증 여부 반환 [true : 인증 성공, false : 인증 실패] 반환", content = @Content(schema = @Schema(implementation = VerificationDto.class)))
     })
-    @PostMapping("/email/verification")
+    @PostMapping("/verify-email")
     public VerificationDto executeEmailVerification(@RequestBody final EmailVerificationRequest request) {
         return emailVerificationService.execute(request);
     }
 
     @Operation(summary = "이메일 인증코드 발급 API", description = "호출 성공 시 이메일 인증코드 발급")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "이메일 인증코드 발급, String type", content = @Content(schema = @Schema(implementation = String.class)))
+            @ApiResponse(responseCode = "200", description = "이메일 인증코드 발급", content = @Content(schema = @Schema(implementation = HttpStatus.class)))
     })
-    @PostMapping("/email")
+    @PostMapping("/send-verification-email")
     public void createEmailVerification(@RequestBody final EmailRequest emailRequest) {
         issuanceVerificationCode.execute(emailRequest);
     }
