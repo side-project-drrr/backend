21bd86c75815142b73ed6c010738ea1a634a429a
YoungKyonYou <man.spride1324@gmail.com>
2024-02-21 00:05:23 +0900

    feat: 객체를 byte[]로 저장 하고 객체로 deserialize 해서 가져오는 기능 추가

--- drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/service/RedisTechBlogPostService.java
+++ drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/service/RedisTechBlogPostService.java
@@ -1,5 +1,9 @@
 package com.drrr.domain.techblogpost.service;
 
+import com.drrr.domain.redis.util.ObjectBytesConverter;
+import com.drrr.domain.techblogpost.dto.TechBlogPostCategoryDto;
+import com.drrr.domain.techblogpost.entity.RedisAllPostCategoriesSlice;
+import com.drrr.domain.techblogpost.entity.RedisAllPostCategoriesSlice.CompoundPostCategoriesSliceId;
 import com.drrr.domain.techblogpost.entity.RedisCategoryTechBlogPost;
 import com.drrr.domain.techblogpost.entity.RedisTechBlogPost;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
@@ -8,7 +12,11 @@ import com.drrr.domain.techblogpost.repository.RedisTechBlogPostRepository;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +28,40 @@ public class RedisTechBlogPostService {
     private final RedisTechBlogPostRepository redisTechBlogPostRepository;
     private final RedisCategoryTechBlogPostRepository redisCategoryTechBlogPostRepository;
     private final RedisTemplate<String, RedisTechBlogPost> redisTechBlogPostTemplate;
+    private final RedisTemplate<CompoundPostCategoriesSliceId, byte[]> redisTemplate;
+    private final ObjectBytesConverter objectBytesConverter;
+
+    public Slice<TechBlogPostCategoryDto> findAllPostsInRedis(final int page, final int size) {
+        CompoundPostCategoriesSliceId key = CompoundPostCategoriesSliceId.builder()
+                .page(page)
+                .size(size)
+                .build();
+
+        byte[] value = redisTemplate.opsForValue().get(key);
+
+        if (Objects.isNull(value)) {
+            return null;
+        }
+
+        RedisAllPostCategoriesSlice entity = (RedisAllPostCategoriesSlice) objectBytesConverter.toObject(value);
+        return new SliceImpl<>(entity.getSliceData(), PageRequest.of(page, size), entity.isHasNext());
+    }
+
+    //Redis에 저장할 건데 key는 String value는 RedisAllPostCategoriesSlice를 byte[]로 변환한 다음에 문자열로 value로 저장할거야
+    //일단 Slice<TechBlogPostCategoryDto>를 byte로 만들고 String으로 저장해줘
+    public void saveAllPostsInRedis(final int page, final int size, final boolean hasNext,
+                                    final List<TechBlogPostCategoryDto> posts) {
+        CompoundPostCategoriesSliceId key = CompoundPostCategoriesSliceId.builder().page(page).size(size).build();
+        RedisAllPostCategoriesSlice redisAllPostCategoriesSlice = RedisAllPostCategoriesSlice.builder()
+                .id(key)
+                .sliceData(posts)
+                .hasNext(hasNext)
+                .build();
+
+        byte[] serialize = objectBytesConverter.toBytes(redisAllPostCategoriesSlice);
+        redisTemplate.opsForValue().set(key, serialize, 3600, TimeUnit.SECONDS);
+    }
+
 
     public List<TechBlogPost> findPostsByIdsInRedis(final List<Long> postIds) {
         //redis repository에서는 찾고자하는 데이터가 없으면 빈 리스트 대신 null를 반환함
