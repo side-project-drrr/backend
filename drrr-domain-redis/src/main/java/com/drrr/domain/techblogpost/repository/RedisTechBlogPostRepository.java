236eb5dea244bc9d89805cc29da5f72b7e88240a
YoungKyonYou <man.spride1324@gmail.com>
2024-02-26 21:39:17 +0900

    refactor: 파라미터에 final 적용

--- drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/repository/RedisTechBlogPostRepository.java
+++ drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/repository/RedisTechBlogPostRepository.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RedisTechBlogPostRepository extends CrudRepository<RedisTechBlogPost, Long> {
-    Optional<List<RedisTechBlogPost>> findByIdIn(List<Long> postIds);
+    Optional<List<RedisTechBlogPost>> findByIdIn(final List<Long> postIds);
 
-    Optional<RedisTechBlogPost> findById(Long postIds);
+    Optional<RedisTechBlogPost> findById(final Long postIds);
 
 }
