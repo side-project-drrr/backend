abcd9f6fd20dc8db840dccadceeb3c924fa185fb
YoungKyonYou <man.spride1324@gmail.com>
2024-03-01 13:41:37 +0900

    feat: 오늘 추천한 게시물에서 읽지 않는 게시물들을 가져오는 repository 추가

--- drrr-domain/src/main/java/com/drrr/domain/log/repository/CustomMemberPostLogRepository.java
+++ drrr-domain/src/main/java/com/drrr/domain/log/repository/CustomMemberPostLogRepository.java
@@ -1,9 +1,8 @@
 package com.drrr.domain.log.repository;
 
-import com.drrr.domain.log.entity.post.MemberPostLog;
 import java.util.List;
 
 public interface CustomMemberPostLogRepository {
-    List<MemberPostLog> findByMemberPostLog(final Long memberId, final List<Long> postIds);
+    List<Long> findTodayUnreadRecommendPostIds(final Long memberId);
 
 }
