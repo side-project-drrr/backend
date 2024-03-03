56ab6c895017f103037d7aca6799b97e5b8618c0
JaehongDev <println@kakao.com>
2024-03-04 01:03:59 +0900

    refactor: 도메인 책임 분리

--- drrr-domain/src/main/java/com/drrr/domain/category/service/InitializeWeightService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/InitializeWeightService.java
@@ -1,15 +1,11 @@
 package com.drrr.domain.category.service;
 
-import com.drrr.core.recommandation.constant.WeightConstants;
-import com.drrr.domain.category.entity.Category;
-import com.drrr.domain.category.entity.CategoryWeight;
+import com.drrr.domain.category.domain.Categories;
 import com.drrr.domain.category.repository.CategoryRepository;
 import com.drrr.domain.category.repository.CategoryWeightRepository;
 import com.drrr.domain.member.entity.Member;
-import com.drrr.domain.member.repository.MemberRepository;
-import java.time.LocalDateTime;
+import com.drrr.domain.member.service.SearchMemberService;
 import java.util.List;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,37 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class InitializeWeightService {
     private final CategoryWeightRepository categoryWeightRepository;
-    private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
+    private final SearchMemberService searchMemberService;
 
     public void initializeCategoryWeight(final Long memberId, final List<Long> categories) {
-        List<Category> categoryList = categoryRepository.findIds(categories);
+        Categories categoryList = new Categories(categoryRepository.findIds(categories));
+        Member member = searchMemberService.execute(memberId);
 
-        if (categories.isEmpty()) {
-            log.error(
-                    "InitializeWeightService Class initializeCategoryWeight(final Long memberId, final List<Long> categories) Method RuntimeException Error");
-            throw new RuntimeException(
-                    "InitializeWeightService.initializeCategoryWeight() - Cannot find such categories");
-        }
-
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    log.error(
-                            "initializeCategoryWeight(final Long memberId, final List<Long> categories) Method NoSuchElementException Error");
-                    return new NoSuchElementException(
-                            "InitializeWeightService.initializeCategoryWeight() - No Member Weight found with memberId: "
-                                    + memberId);
-                });
-        List<CategoryWeight> initialCategoryWeight = categoryList.stream()
-                .map(category -> CategoryWeight.builder()
-                        .member(member)
-                        .category(category)
-                        .weightValue(WeightConstants.MIN_CONDITIONAL_WEIGHT.getValue())
-                        .lastReadAt(LocalDateTime.now())
-                        .preferred(true)
-                        .build()).toList();
-
-        categoryWeightRepository.saveAll(initialCategoryWeight);
+        categoryWeightRepository.saveAll(categoryList.toCategoryWeights(member));
     }
 
 
