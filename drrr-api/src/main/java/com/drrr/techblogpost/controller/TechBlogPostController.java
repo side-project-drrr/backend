9660310e362c89c7a4faa04f5274e665561ff28d
YoungKyonYou <man.spride1324@gmail.com>
2024-03-03 19:41:27 +0900

    feat: 탑 조회수나 좋아요 가져오는 api에서 External Service 명 변경

--- drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostController.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostController.java
@@ -6,6 +6,7 @@ import com.drrr.domain.techblogpost.dto.TechBlogPostCategoryDto;
 import com.drrr.domain.techblogpost.dto.TechBlogPostDetailedInfoDto;
 import com.drrr.techblogpost.service.ExternalTechBlogPostSearchService;
 import com.drrr.techblogpost.service.ExternalTechBlogPostService;
+import com.drrr.techblogpost.service.ExternalTopTechBlogPostService;
 import com.drrr.web.page.request.PageableRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -34,6 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TechBlogPostController {
     private final ExternalTechBlogPostService externalTechBlogPostService;
     private final ExternalTechBlogPostSearchService externalTechBlogPostSearchService;
+    private final ExternalTopTechBlogPostService externalTopTechBlogPostService;
 
     @Operation(summary = "모든 기술 블로그 정보를 가져오는 API", description = """
             호출 성공 시 모든 기술 블로그 정보 반환 [page 값은 0부터 시작 
@@ -96,6 +98,6 @@ public class TechBlogPostController {
     @GetMapping("/posts/top/{type}/{count}")
     public List<TechBlogPostCategoryDto> findTopNPosts(@NotNull @PathVariable("count") final int count,
                                                        @NotNull @PathVariable("type") final TopTechBlogType type) {
-        return externalTechBlogPostService.execute(count, type);
+        return externalTopTechBlogPostService.execute(count, type);
     }
 }
--- drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalTechBlogPostService.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalTechBlogPostService.java
@@ -1,6 +1,5 @@
 package com.drrr.techblogpost.service;
 
-import com.drrr.core.code.techblog.TopTechBlogType;
 import com.drrr.domain.techblogpost.dto.TechBlogPostCategoryDto;
 import com.drrr.domain.techblogpost.dto.TechBlogPostDetailedInfoDto;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
@@ -8,7 +7,6 @@ import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
 import com.drrr.domain.techblogpost.service.RedisTechBlogPostService;
 import com.drrr.domain.techblogpost.service.TechBlogPostService;
 import com.drrr.web.page.request.PageableRequest;
-import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
@@ -20,12 +18,6 @@ public class ExternalTechBlogPostService {
     private final TechBlogPostService techBlogPostService;
     private final RedisTechBlogPostService redisTechBlogPostService;
     private final TechBlogPostRepository techBlogPostRepository;
-
-    public List<TechBlogPostCategoryDto> execute(final int count, final TopTechBlogType type) {
-
-        return techBlogPostService.findTopPost(count, type);
-    }
-
 
     public Slice<TechBlogPostCategoryDto> execute(final PageableRequest pageableRequest) {
         return techBlogPostRepository.findAllPosts(pageableRequest.fromPageRequest());
