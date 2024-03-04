52f2c75bf2d151e3670a29c744e3fa9814e5a237
JaehongDev <println@kakao.com>
2024-03-04 18:31:47 +0900

    refactor: 이메일 중복 검사 기능 리펙터링

--- drrr-domain/src/main/java/com/drrr/domain/email/service/VerificationService.java
+++ drrr-domain/src/main/java/com/drrr/domain/email/service/VerificationService.java
@@ -1,12 +1,12 @@
 package com.drrr.domain.email.service;
 
 import com.drrr.domain.email.entity.Email;
+import com.drrr.domain.email.generator.EmailCodeGenerator;
 import com.drrr.domain.email.repository.EmailRepository;
 import com.drrr.domain.exception.DomainExceptionCode;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.UUID;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,42 +18,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 public class VerificationService {
-    private final EmailRepository emailRepository;
-    private static final int END_INDEX = 6;
     private static final int THREE_MINUTES = 180;
 
-    public String createVerificationCode(final String providerId, final String email) {
-        boolean codeExists = emailRepository.existsByProviderId(providerId);
+    private final EmailCodeGenerator emailCodeGenerator;
+    private final EmailRepository emailRepository;
 
+    public String createVerificationCode(final String providerId, final String email) {
         //인증번호를 중복으로 요청하는 경우 기존 것 삭제
-        if (codeExists) {
+        if (emailRepository.existsByProviderId(providerId)) {
             emailRepository.deleteByProviderId(providerId);
         }
 
-        final UUID uuid = UUID.randomUUID();
-        final String code = uuid.toString().replace("-", "").substring(0, END_INDEX);
-
         //isVerified : 프론트에게 추후 인증코드가 유효한지 여부 전달하기 위함
-        final Email emailEntity = Email.builder()
+        return emailRepository.save(Email.builder()
                 .email(email)
-                .verificationCode(code)
+                .verificationCode(emailCodeGenerator.generate())
                 .isVerified(false)
                 .providerId(providerId)
-                .build();
-        emailRepository.save(emailEntity);
-        return code;
+                .build()).getVerificationCode();
     }
 
     public VerificationDto verifyCode(final String providerId, final String verificationCode) {
         final Email email = emailRepository.findByProviderId(providerId).orElseThrow(() -> {
-            log.error("provider id로 이메일 인증 정보를 찾을 수 없습니다.");
-            log.error("providerId -> {}", providerId);
+            log.error("provider id로 이메일 인증 정보를 찾을 수 없습니다. providerId -> {}", providerId);
             return DomainExceptionCode.EMAIL_VERIFICATION_INFORMATION_NOT_FOUND.newInstance();
         });
 
         final Duration duration = Duration.between(email.getCreatedAt(), LocalDateTime.now());
         final long differenceInSeconds = duration.toSeconds();
-        boolean isVerified = Objects.equals(email.getVerificationCode(), verificationCode);
+        final boolean isVerified = Objects.equals(email.getVerificationCode(), verificationCode);
 
         //3분 안에 입력
         if (differenceInSeconds > THREE_MINUTES) {
