f13196f1ef0910ef1b2dc59ada5b9e84e9659b04
JaehongDev <println@kakao.com>
2024-03-04 19:50:10 +0900

    refactor: 로그 출력 하나로 합치기

--- drrr-domain/src/main/java/com/drrr/domain/like/service/TechBlogPostLikeService.java
+++ drrr-domain/src/main/java/com/drrr/domain/like/service/TechBlogPostLikeService.java
@@ -4,7 +4,7 @@ import com.drrr.domain.exception.DomainExceptionCode;
 import com.drrr.domain.like.entity.TechBlogPostLike;
 import com.drrr.domain.like.repository.TechBlogPostLikeRepository;
 import com.drrr.domain.member.entity.Member;
-import com.drrr.domain.member.repository.MemberRepository;
+import com.drrr.domain.member.service.SearchMemberService;
 import com.drrr.domain.techblogpost.entity.TechBlogPost;
 import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +18,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TechBlogPostLikeService {
     private final TechBlogPostLikeRepository postLikeRepository;
-    private final MemberRepository memberRepository;
     private final TechBlogPostRepository techBlogPostRepository;
+    private final SearchMemberService searchMemberService;
 
     public void addPostLike(final Long memberId, final Long postId) {
-        final Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    log.error("사용자를 찾을 수 없습니다.");
-                    log.error("memberId -> {}", memberId);
-                    return DomainExceptionCode.MEMBER_NOT_FOUND.newInstance();
-                });
+        final Member member = searchMemberService.execute(memberId);
 
         final TechBlogPost post = techBlogPostRepository.findByIdWithPessimisticLock(postId)
                 .orElseThrow(() -> {
@@ -37,10 +32,8 @@ public class TechBlogPostLikeService {
                 });
 
         postLikeRepository.findByPostIdAndMemberId(memberId, postId)
-                .ifPresentOrElse((__) -> {
-                    log.error("memberId -> {}", memberId);
-                    log.error("postId -> {}", postId);
-                    log.error("사용자가 하나의 게시물에 중복으로 좋아요를 눌렀습니다.");
+                .ifPresentOrElse((postLike) -> {
+                    log.error("사용자가 하나의 게시물에 중복으로 좋아요를 눌렀습니다. memberId -> {}, postId ->{} ", memberId, postId);
                     throw DomainExceptionCode.DUPLICATE_LIKE.newInstance();
                 }, () -> {
                     final TechBlogPostLike like = TechBlogPostLike.builder()
@@ -55,13 +48,11 @@ public class TechBlogPostLikeService {
     public void deletePostLike(final Long memberId, final Long postId) {
         final TechBlogPost post = techBlogPostRepository.findByIdWithPessimisticLock(postId)
                 .orElseThrow(() -> {
-                    log.error("기술블로그를 찾을 수 없습니다.");
-                    log.error("postId -> {}", postId);
+                    log.error("기술블로그를 찾을 수 없습니다. postId -> {}", postId);
                     return DomainExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
                 });
 
         postLikeRepository.deleteByMemberIdAndPostId(memberId, postId);
-
         post.decreaseLikeCount();
     }
 }
