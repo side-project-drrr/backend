52fd812aeb51dd8d8737b71b91d4f526520c7063
JaehongDev <println@kakao.com>
2024-03-10 22:19:23 +0900

    feat: 당근마켓 기술블로그 크롤러 추가

--- drrr-batch/src/main/java/com/drrr/reader/CrawlerItemReaderFactory.java
+++ drrr-batch/src/main/java/com/drrr/reader/CrawlerItemReaderFactory.java
@@ -3,6 +3,7 @@ package com.drrr.reader;
 import com.drrr.core.code.techblog.TechBlogCode;
 import com.drrr.domain.ExternalBlogPosts;
 import com.drrr.reader.impl.BespinGlobalCrawlerItemReader;
+import com.drrr.reader.impl.DaangnCrawlerItemReader;
 import com.drrr.reader.impl.DevOceanCrawlerItemReader;
 import com.drrr.reader.impl.DevSistersCrawlerItemReader;
 import com.drrr.reader.impl.KakaoCrawlerItemReader;
@@ -42,6 +43,7 @@ public class CrawlerItemReaderFactory {
             case LINE -> LineCrawlerItemReader::new;
             case DEV_SISTERS -> DevSistersCrawlerItemReader::new;
             case BESPIN_GLOBAL -> BespinGlobalCrawlerItemReader::new;
+            case DAANGN -> DaangnCrawlerItemReader::new;
         };
     }
 }
