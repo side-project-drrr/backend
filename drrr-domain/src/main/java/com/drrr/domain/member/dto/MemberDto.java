fced6e7c6f83f9199fb710bad082039ac0c256bb
JaehongDev <println@kakao.com>
2024-03-11 19:19:45 +0900

    refactor: 사용하지 않는 쿼리 프로젝션 최소화

--- drrr-domain/src/main/java/com/drrr/domain/member/dto/MemberDto.java
+++ drrr-domain/src/main/java/com/drrr/domain/member/dto/MemberDto.java
@@ -1,7 +1,6 @@
 package com.drrr.domain.member.dto;
 
 import com.drrr.domain.member.entity.Member;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 
 @Builder
@@ -13,16 +12,6 @@ public record MemberDto(
         String provider,
         String providerId
 ) {
-    @QueryProjection
-    public MemberDto(Long id, String email, String nickname, String profileImageUrl, String provider,
-                     String providerId) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-        this.profileImageUrl = profileImageUrl;
-        this.provider = provider;
-        this.providerId = providerId;
-    }
 
     public static MemberDto toDto(Member member) {
         return MemberDto.builder()
