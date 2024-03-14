b2667ed95b379681e1bc914d132ffd7b85d407ce
YoungKyonYou <man.spride1324@gmail.com>
2024-03-15 01:19:00 +0900

    test: fixture 메서드 명 변경

--- drrr-domain/src/test/java/com/drrr/domain/member/service/MemberViewWeightServiceTest.java
+++ drrr-domain/src/test/java/com/drrr/domain/member/service/MemberViewWeightServiceTest.java
@@ -171,7 +171,7 @@ class MemberViewWeightServiceTest extends ServiceIntegrationTest {
         Member member = MemberFixture.createMember();
         memberRepository.save(member);
 
-        TechBlogPost post = TechBlogPostFixture.createTechBlogPostLike(VIEW_COUNT);
+        TechBlogPost post = TechBlogPostFixture.createTechBlogPostView(VIEW_COUNT);
         techBlogPostRepository.save(post);
 
         Category category = CategoryFixture.createCategory();
