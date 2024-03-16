e563e7144f3315e36d57cb58c1c4b61ad9b7c6d9
YoungKyonYou <man.spride1324@gmail.com>
2024-03-16 11:47:01 +0900

    test: fixture 클래스 추가 및 gradle 의존성 수정 -> drrr-domain 모듈의 test 패키지에 작성된 클래스를 drrr-api test 패키지에서 의존하게 변경

---
+++ drrr-domain/src/test/java/com/drrr/domain/fixture/post/TechBlogPostCategoryFixture.java
@@ -0,0 +1,57 @@
+package com.drrr.domain.fixture.post;
+
+import com.drrr.domain.category.entity.Category;
+import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import com.drrr.domain.techblogpost.entity.TechBlogPostCategory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TechBlogPostCategoryFixture {
+
+    public static List<TechBlogPostCategory> createTechBlogPostCategories(final List<TechBlogPost> posts,
+                                                                          final List<Category> categories) {
+
+        List<TechBlogPostCategory> techBlogPostCategories = new ArrayList<>();
+
+        posts.forEach(post -> {
+            List<TechBlogPostCategory> techBlogPostCategoryList = categories.stream()
+                    .map(category -> TechBlogPostCategory.builder()
+                            .post(post)
+                            .category(category)
+                            .build()).toList();
+            techBlogPostCategories.addAll(techBlogPostCategoryList);
+        });
+        return techBlogPostCategories;
+    }
+
+    public static List<TechBlogPostCategory> createTechBlogPostCategories(final List<TechBlogPost> posts,
+                                                                          final Category category) {
+
+        List<TechBlogPostCategory> techBlogPostCategories = new ArrayList<>();
+
+        posts.forEach(post -> {
+            TechBlogPostCategory techBlogPostCategoryList = TechBlogPostCategory.builder()
+                    .post(post)
+                    .category(category)
+                    .build();
+            techBlogPostCategories.add(techBlogPostCategoryList);
+        });
+        return techBlogPostCategories;
+    }
+
+    public static List<TechBlogPostCategory> createTechBlogPostCategories(final TechBlogPost post,
+                                                                          final List<Category> categories) {
+        return categories.stream()
+                .map(category -> TechBlogPostCategory.builder()
+                        .post(post)
+                        .category(category)
+                        .build()).toList();
+    }
+
+    public static TechBlogPostCategory createTechBlogPostCategory(final TechBlogPost post, final Category category) {
+        return TechBlogPostCategory.builder()
+                .post(post)
+                .category(category)
+                .build();
+    }
+}
---
+++ drrr-domain/src/test/java/com/drrr/domain/techblogpost/service/FindPostServiceTest.java
@@ -0,0 +1,142 @@
+package com.drrr.domain.techblogpost.service;
+
+
+import com.drrr.domain.category.entity.Category;
+import com.drrr.domain.category.repository.CategoryRepository;
+import com.drrr.domain.category.repository.CategoryWeightRepository;
+import com.drrr.domain.fixture.category.CategoryFixture;
+import com.drrr.domain.fixture.category.weight.CategoryWeightFixture;
+import com.drrr.domain.fixture.member.MemberFixture;
+import com.drrr.domain.fixture.post.TechBlogPostCategoryFixture;
+import com.drrr.domain.fixture.post.TechBlogPostFixture;
+import com.drrr.domain.member.entity.Member;
+import com.drrr.domain.member.repository.MemberRepository;
+import com.drrr.domain.techblogpost.dto.TechBlogPostBasicInfoDto;
+import com.drrr.domain.techblogpost.dto.TechBlogPostCategoryDto;
+import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import com.drrr.domain.techblogpost.entity.TechBlogPostCategory;
+import com.drrr.domain.techblogpost.repository.TechBlogPostCategoryRepository;
+import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
+import com.drrr.domain.util.ServiceIntegrationTest;
+import jakarta.persistence.EntityManager;
+import java.util.Comparator;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class FindPostServiceTest extends ServiceIntegrationTest {
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private TechBlogPostRepository techBlogPostRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private CategoryWeightRepository categoryWeightRepository;
+    @Autowired
+    private TechBlogPostCategoryRepository techBlogPostCategoryRepository;
+    @Autowired
+    private EntityManager em;
+    final static int POST_COUNTS = 20;
+    final static int CATEGORY_COUNT = 10;
+
+
+    @Test
+    void 게시글이_슬라이스로_정상적으로_조회됩니다() {
+        //given
+        Member member = MemberFixture.createMember();
+        memberRepository.save(member);
+
+        List<TechBlogPost> posts = TechBlogPostFixture.createTechBlogPosts(POST_COUNTS);
+        List<Category> categories = CategoryFixture.createCategories(CATEGORY_COUNT);
+
+        techBlogPostRepository.saveAll(TechBlogPostFixture.createTechBlogPosts(POST_COUNTS));
+        categoryRepository.saveAll(CategoryFixture.createCategories(CATEGORY_COUNT));
+
+        techBlogPostCategoryRepository.saveAll(
+                TechBlogPostCategoryFixture.createTechBlogPostCategories(posts, categories)
+        );
+
+        categoryWeightRepository.saveAll(CategoryWeightFixture.createCategoryWeights(member, categories));
+
+        em.clear();
+        em.flush();
+
+        // when
+        Slice<TechBlogPostCategoryDto> allPosts = techBlogPostRepository.findAllPosts(PageRequest.of(0, 10));
+
+        // then
+        List<TechBlogPostBasicInfoDto> basicInfoDtos = allPosts.getContent().stream()
+                .map(TechBlogPostCategoryDto::techBlogPostBasicInfoDto)
+                .toList();
+        Comparator<TechBlogPostBasicInfoDto> writtenAtComparator = Comparator.comparing(
+                        TechBlogPostBasicInfoDto::writtenAt)
+                .reversed();
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(allPosts.hasNext()),
+                () -> Assertions.assertTrue(allPosts.isFirst()),
+                () -> Assertions.assertFalse(allPosts.isLast()),
+                () -> Assertions.assertEquals(0, allPosts.getNumber()),
+                () -> Assertions.assertEquals(10, allPosts.getNumberOfElements()),
+                () -> Assertions.assertEquals(10, allPosts.getSize()),
+                () -> Assertions.assertEquals(basicInfoDtos.stream().sorted(writtenAtComparator).toList(),
+                        basicInfoDtos)
+        );
+
+    }
+
+    @Test
+    void 카테고리로_게시글_슬라이스_조회가_정상적으로_조회됩니다() {
+        //given
+        Member member = MemberFixture.createMember();
+        memberRepository.save(member);
+
+        List<TechBlogPost> posts = TechBlogPostFixture.createTechBlogPosts(POST_COUNTS);
+        techBlogPostRepository.saveAll(posts);
+
+        Category category = CategoryFixture.createCategory();
+        categoryRepository.save(category);
+
+        List<TechBlogPostCategory> techBlogPostCategory = TechBlogPostCategoryFixture.createTechBlogPostCategories(
+                posts,
+                category);
+        techBlogPostCategoryRepository.saveAll(
+                techBlogPostCategory
+        );
+
+        categoryWeightRepository.save(CategoryWeightFixture.createCategoryWeight(member, category));
+
+        em.clear();
+        em.flush();
+
+        // when
+        Slice<TechBlogPostCategoryDto> allPosts = techBlogPostRepository.findPostsByCategory(
+                category.getId(),
+                PageRequest.of(0, 10)
+        );
+
+        // then
+        List<TechBlogPostBasicInfoDto> basicInfoDtos = allPosts.getContent().stream()
+                .map(TechBlogPostCategoryDto::techBlogPostBasicInfoDto)
+                .toList();
+        Comparator<TechBlogPostBasicInfoDto> writtenAtComparator = Comparator.comparing(
+                        TechBlogPostBasicInfoDto::writtenAt)
+                .reversed();
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(allPosts.hasNext()),
+                () -> Assertions.assertTrue(allPosts.isFirst()),
+                () -> Assertions.assertFalse(allPosts.isLast()),
+                () -> Assertions.assertEquals(0, allPosts.getNumber()),
+                () -> Assertions.assertEquals(10, allPosts.getNumberOfElements()),
+                () -> Assertions.assertEquals(10, allPosts.getSize()),
+                () -> Assertions.assertEquals(basicInfoDtos.stream().sorted(writtenAtComparator).toList(),
+                        basicInfoDtos));
+    }
+}
