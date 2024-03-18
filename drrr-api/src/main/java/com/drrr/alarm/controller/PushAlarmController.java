155a6a6ccf7f0fcab11b1c9478fb8212bc82b6cd
YoungKyonYou <man.spride1324@gmail.com>
2024-03-18 21:42:05 +0900

    feat: push 관련 api 추가

--- drrr-api/src/main/java/com/drrr/alarm/controller/PushAlarmController.java
+++ drrr-api/src/main/java/com/drrr/alarm/controller/PushAlarmController.java
@@ -26,6 +26,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,10 +51,10 @@ public class PushAlarmController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "날짜 범위 pushDate(format : YYYYMMDD)에 해당하는 웹 푸시 상태 정보 반환")
     })
-    @GetMapping("/members/me/web-push/posts/count")
+    @GetMapping("/members/me/web-push/posts/count/{count}")
     public List<PushDateDto> findMemberPushPostsCountByDate(@MemberId final Long memberId,
-                                                            @Valid @ModelAttribute final PushDateRequest request) {
-        return externalSearchPushPostsCountService.execute(memberId, request);
+                                                            @PathVariable("count") final int count) {
+        return externalSearchPushPostsCountService.execute(memberId, count);
     }
 
     @Operation(summary = "날짜 범위에 해당하는 사용자의 웹 푸시 게시물 정보 반환 API - [JWT TOKEN REQUIRED]",
@@ -85,17 +86,6 @@ public class PushAlarmController {
     @PostMapping("/members/me/web-push/posts/open")
     public void updateMemberPushReadStatus(@MemberId final Long memberId, @RequestBody final List<LocalDate> pushDates) {
         externalUpdatePushOpenStatusService.execute(memberId, pushDates);
-    }
-
-    @Operation(summary = "지정된 날짜에 해당하는 사용자 웹 푸시 블로그 정보 반환  API - [JWT TOKEN REQUIRED]",
-            description = "호출 성공 시 지정된 pushDate(format : YYYYMMDD)에 푸시 해준 사용자의 블로그 정보 반환(작성일 내림차순)")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "지정된 pushDate(format : YYYYMMDD)에 웹 푸시를 해준 사용자의 블로그 정보 반환")
-    })
-    @GetMapping("/members/me/web-push/posts")
-    public List<TechBlogPostCategoryDto> findMemberPushPosts(@MemberId final Long memberId,
-                                                             @RequestParam("pushDate") final LocalDate pushDate) {
-        return externalSearchPushPostsService.execute(memberId, pushDate);
     }
 
     @Operation(summary = "사용자 웹 푸시 구독 정보 저장 API - [JWT TOKEN REQUIRED]",
