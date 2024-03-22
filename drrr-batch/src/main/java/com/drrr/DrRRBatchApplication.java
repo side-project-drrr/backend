7493144e7ffd0e9bc6479f042ed80ab1a6c7be7f
JaehongDev <println@kakao.com>
2024-03-22 19:09:22 +0900

    feat: ProfileScan 애노테이션 등록

--- drrr-batch/src/main/java/com/drrr/DrRRBatchApplication.java
+++ drrr-batch/src/main/java/com/drrr/DrRRBatchApplication.java
@@ -2,11 +2,14 @@ package com.drrr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.drrr")
 public class DrRRBatchApplication {
 
     public static void main(String[] args) {
+
         System.exit(SpringApplication.exit(SpringApplication.run(DrRRBatchApplication.class, args)));
     }
 
