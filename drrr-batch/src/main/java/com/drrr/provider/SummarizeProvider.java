111506ed7e692a9d8313e95b3c495ba13f609328
JaehongDev <println@kakao.com>
2024-03-22 19:10:38 +0900

    feat: 문단 요약 기능 구현

---
+++ drrr-batch/src/main/java/com/drrr/provider/SummarizeProvider.java
@@ -0,0 +1,46 @@
+package com.drrr.provider;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+import com.drrr.property.TextSummarizerProperty;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SummarizeProvider {
+
+    private final ChildProcessRunner childProcessRunner;
+    private final TextSummarizerProperty textSummarizerProperty;
+
+
+    public String execute(List<String> texts) {
+        var executor = Executors.newFixedThreadPool(textSummarizerProperty.calculateRunnerCount(texts.size()));
+
+        var completableFutures = texts.stream()
+                .filter(text -> !text.isBlank())
+                .map(text -> text.replaceAll("\0", ""))
+                .map((text) -> supplyAsync(() -> this.executePythonScript(text), executor))
+                .toList();
+
+        return completableFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.joining());
+    }
+
+    private String executePythonScript(String args) {
+        log.info("{}", textSummarizerProperty);
+        return this.childProcessRunner.execute(
+                "python",
+                textSummarizerProperty.scriptPath(),
+                args
+        );
+    }
+
+}
