ac5566ae2627397c53d02867461aaa86390331a8
JaehongDev <println@kakao.com>
2024-03-25 14:15:22 +0900

    test: 여러 페이지를 크롤링하는 Pages 테스트 코드 작성

---
+++ drrr-fluent-crawler/src/test/java/com/drrr/fluent/cralwer/core/PagesTest.java
@@ -0,0 +1,37 @@
+package com.drrr.fluent.cralwer.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.drrr.fluent.cralwer.core.PaginationReader.PaginationInformation;
+import com.drrr.fluent.cralwer.util.FakeWebDriver;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PagesTest {
+
+    @Test
+    void _5번의_페이지_호출_후_null을_반환합니다() {
+
+        var pages = Pages.<String>builder()
+                .contentsReader(webDriver -> "")
+                .paginationReader(webDriver -> new PaginationInformation(5))
+                .webDriver(new FakeWebDriver())
+                .build();
+
+        assertAll(
+                () -> assertThat(pages.execute()).isEqualTo(""),
+                () -> assertThat(pages.execute()).isEqualTo(""),
+                () -> assertThat(pages.execute()).isEqualTo(""),
+                () -> assertThat(pages.execute()).isEqualTo(""),
+                () -> assertThat(pages.execute()).isEqualTo(""),
+
+                () -> assertThat(pages.execute()).isEqualTo(null)
+        );
+
+    }
+
+}
