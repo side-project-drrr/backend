2c621d1b9652fdcd9215396794bab05ec1a4fdb2
JaehongDev <println@kakao.com>
2024-03-27 21:42:04 +0900

    test: SquareLab 테스트코드 추가

---
+++ drrr-batch/src/test/java/com/drrr/reader/fluent/blog/SquareLabCrawlerTest.java
@@ -0,0 +1,28 @@
+package com.drrr.reader.fluent.blog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.drrr.reader.AbstractCrawlerPageItemReader.CrawlingLocalDatePatterns;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class SquareLabCrawlerTest {
+
+
+    @Test
+    void local() {
+        var str = "2023년 8월 30일";
+
+        var date = CrawlingLocalDatePatterns.PATTERN8.parse(str);
+
+        assertThat(date.getYear()).isEqualTo(2023);
+        assertThat(date.getMonthValue()).isEqualTo(8);
+    }
+
+    @Test
+    @Disabled
+    void 크롤링_테스트() {
+        new SquareLabCrawler().pageItemReader(new FirefoxDriver()).read();
+    }
+}
