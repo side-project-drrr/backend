378e3b09c8b6f2fee6bc7fa8c5206c51f4a49c62
JaehongDev <println@kakao.com>
2024-03-28 13:10:08 +0900

    feat: WebDriverPool 구현

---
+++ drrr-fluent-crawler/src/main/java/com/drrr/fluent/cralwer/core/GenericParallelPages.java
@@ -0,0 +1,95 @@
+package com.drrr.fluent.cralwer.core;
+
+import com.drrr.fluent.cralwer.core.PaginationReader.PaginationInformation;
+import com.drrr.fluent.cralwer.core.SinglePage.Mode;
+import com.drrr.fluent.cralwer.core.SinglePage.SinglePageBuilder;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import lombok.Builder;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class GenericParallelPages<T> implements MultiPage<T> {
+    private static final int PARALLEL_COUNT = 2;
+    private final WebDriverPool webDriverPool;
+    private final ParallelPageInitializer pageInitializer;
+    private final ContentsReader<T> contentsReader;
+    private final PaginationReader paginationReader;
+    private final ContentsLoader contentsLoader;
+
+    private int currentPage;
+    private PaginationInformation paginationInformation;
+
+    @Builder
+    public GenericParallelPages(WebDriverPool webDriverPool, ParallelPageInitializer pageInitializer,
+                                ContentsReader<T> contentsReader, PaginationReader paginationReader,
+                                ContentsLoader contentsLoader) {
+        this.webDriverPool = webDriverPool;
+        this.pageInitializer = pageInitializer;
+        this.contentsReader = contentsReader;
+        this.paginationReader = paginationReader;
+        this.contentsLoader = contentsLoader;
+    }
+
+    @Override
+    public List<T> execute() {
+
+        var initializeUrl = pageInitializer.getUrl();
+
+        if (Objects.nonNull(paginationInformation) && paginationInformation.complete(currentPage)) {
+            return Collections.emptyList();
+        }
+
+        preLoaded(initializeUrl.home());
+
+        var runnerCount = Math.min(
+                paginationInformation.remainPage(currentPage),
+                PARALLEL_COUNT
+        );
+        var urlGenerator = initializeUrl.searchUrlGenerator();
+
+        var results = IntStream.iterate(currentPage, i -> i + 1)
+                .parallel()
+                .limit(runnerCount)
+                .mapToObj(urlGenerator::apply)
+                .map((url) -> createSinglePage(url).execute())
+                .toList();
+
+        currentPage += runnerCount;
+        return results;
+    }
+
+    // 메인 크롤러에서 컨텐츠 로드, 페이지네이션 로드
+    private void preLoaded(String home) {
+        if (Objects.isNull(paginationInformation)) {
+            this.paginationInformation = webDriverPool.delegate(webDriver -> {
+                webDriver.get(home);
+                contentsLoader.waitUntilLoad(new WebDriverWait(
+                        webDriver,
+                        Duration.ofSeconds(3))
+                );
+
+                var paginationInformation = paginationReader.read(webDriver);
+                if (!paginationInformation.hasLastPage()) {
+                    throw new IllegalStateException("last page가 없는 경우 해당 기능을 지원하지 않습니다.");
+                }
+                return paginationInformation;
+            });
+        }
+    }
+
+
+    private Page<T> createSinglePage(String url) {
+        return new SinglePageBuilder<T>()
+                .mode(Mode.POLLING)
+                .singlePageInitializer(() -> url)
+                .contentsReader(contentsReader)
+                .contentsLoader(contentsLoader)
+                .webDriverCleaner(webDriverPool::returnObject)
+                .webDriver(webDriverPool.borrow())
+                .build();
+    }
+}
---
+++ drrr-fluent-crawler/src/main/java/com/drrr/fluent/cralwer/core/WebDriverPool.java
@@ -0,0 +1,66 @@
+package com.drrr.fluent.cralwer.core;
+
+import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.DestroyMode;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.PooledObjectFactory;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class WebDriverPool extends GenericObjectPool<WebDriver> {
+    public WebDriverPool(PooledObjectFactory<WebDriver> factory) {
+        super(factory);
+        this.setMaxTotal(10);
+    }
+
+
+    public <T> T delegate(Function<WebDriver, T> function) {
+        try {
+            var webdriver = this.borrowObject();
+            var result = function.apply(webdriver);
+            this.returnObject(webdriver);
+
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public WebDriver borrow() {
+        try {
+            return this.borrowObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        super.close();
+    }
+
+    @RequiredArgsConstructor
+    public static class WebDriverPoolFactory extends BasePooledObjectFactory<WebDriver> {
+        private final FirefoxOptions firefoxOptions;
+
+        @Override
+        public WebDriver create() {
+            return new FirefoxDriver(firefoxOptions);
+        }
+
+        @Override
+        public PooledObject<WebDriver> wrap(WebDriver webDriver) {
+            return new DefaultPooledObject<>(webDriver);
+        }
+
+        @Override
+        public void destroyObject(PooledObject<WebDriver> p, DestroyMode destroyMode) throws Exception {
+            p.getObject().close();
+        }
+    }
+}
