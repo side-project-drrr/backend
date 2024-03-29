6e447372108c557ba34583eea21406ae621b97d1
JaehongDev <println@kakao.com>
2024-03-29 13:49:10 +0900

    refactor: WebDriverPool 위치 변경

--- drrr-batch/src/main/java/com/drrr/reader/fluent/FluentCrawlerConfiguration.java
+++ drrr-batch/src/main/java/com/drrr/reader/fluent/FluentCrawlerConfiguration.java
@@ -5,8 +5,11 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 
 import com.drrr.core.code.techblog.TechBlogCode;
+import com.drrr.fluent.cralwer.core.WebDriverPool;
+import com.drrr.fluent.cralwer.core.WebDriverPool.WebDriverPoolFactory;
 import java.util.List;
 import java.util.Map;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +25,11 @@ public class FluentCrawlerConfiguration {
                         identity()
                 ));
 
+    }
+
+    @Bean(destroyMethod = "close")
+    WebDriverPool webDriverPool() {
+        var webDriverPoolFactory = new WebDriverPoolFactory(new FirefoxOptions());
+        return new WebDriverPool(webDriverPoolFactory);
     }
 }
