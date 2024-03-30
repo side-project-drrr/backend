0017dc729599e70f6f076683f038cb5911aaf42d
YoungKyonYou <man.spride1324@gmail.com>
2024-03-30 15:11:15 +0900

    test: test를 위한 fixture 및 @Autowired를 Integration 클래스에 추가

--- drrr-domain/src/test/java/com/drrr/domain/util/ServiceIntegrationTest.java
+++ drrr-domain/src/test/java/com/drrr/domain/util/ServiceIntegrationTest.java
@@ -1,11 +1,16 @@
 package com.drrr.domain.util;
 
 
+import com.drrr.domain.category.repository.CategoryRepository;
+import com.drrr.domain.category.repository.CategoryWeightRepository;
 import com.drrr.domain.email.generator.EmailCodeGenerator;
 import com.drrr.domain.jpa.config.JpaConfiguration;
 import com.drrr.domain.jpa.config.QueryDSLConfiguration;
+import com.drrr.domain.member.repository.MemberRepository;
+import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,6 +23,14 @@ import org.springframework.stereotype.Service;
 @Import({QueryDSLConfiguration.class, DatabaseCleaner.class, JpaConfiguration.class})
 public class ServiceIntegrationTest {
 
+    @Autowired
+    protected MemberRepository memberRepository;
+    @Autowired
+    protected CategoryRepository categoryRepository;
+    @Autowired
+    protected  CategoryWeightRepository categoryWeightRepository;
+    @Autowired
+    protected  TechBlogPostRepository techBlogPostRepository;
     // 이메일 랜덤 코드 생성기
     @MockBean
     protected EmailCodeGenerator emailCodeGenerator;
