a0f68cd5f4896a65c0c02c832c1c56b36febfa1e
YoungKyonYou <man.spride1324@gmail.com>
2024-03-31 11:28:42 +0900

    feat: record 로 변경

--- drrr-domain/src/main/java/com/drrr/domain/category/domain/CategoryPostDistribution.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/domain/CategoryPostDistribution.java
@@ -5,23 +5,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Builder.Default;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class CategoryPostDistribution {
-    private Map<Long, Integer> categoryIdToPostCounts;
-    private Set<Long> postIds;
 
+public record CategoryPostDistribution(
+        Map<Long, Integer> categoryIdToPostCounts,
+        Set<Long> postIds
+) {
+    public CategoryPostDistribution(Map<Long, Integer> categoryIdToPostCounts) {
+        this(categoryIdToPostCounts, new HashSet<>());
+    }
     public Set<Long> extractRecommendPostIds(final List<ExtractedPostCategoryDto> extractedPostsCategories, final int requireCount) {
         extractedPostsCategories.forEach(dto -> {
             if (hasPostToDistribute(dto.categoryId()) && postIds.add(dto.postId())) {
