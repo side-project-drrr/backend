28d93b687b5f6f58395d60914e8ed48ba1a9286d
YoungKyonYou <man.spride1324@gmail.com>
2024-03-31 11:13:20 +0900

    refactor: 별도의 서비스로 역할 분리

--- drrr-domain/src/main/java/com/drrr/domain/category/service/RecommendPostService.java
+++ drrr-domain/src/main/java/com/drrr/domain/category/service/RecommendPostService.java
@@ -1,35 +1,26 @@
 package com.drrr.domain.category.service;
 
-import com.drrr.domain.category.domain.CategoryPostDistribution;
-import com.drrr.domain.category.domain.CategoryWeights;
-import com.drrr.domain.category.repository.CategoryWeightRepository;
 import com.drrr.domain.log.repository.MemberPostLogRepository;
-import com.drrr.domain.techblogpost.repository.custom.CustomTechBlogPostCategoryRepositoryImpl;
-import com.drrr.domain.techblogpost.repository.impl.CustomTechBlogPostRepositoryImpl;
 import com.querydsl.core.annotations.QueryProjection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+// external -> 이거 명칭이 애매함
+//
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class RecommendPostService {
-    private final CategoryWeightRepository categoryWeightRepository;
-    private final CustomTechBlogPostRepositoryImpl customTechBlogPostRepository;
-    private final CustomTechBlogPostCategoryRepositoryImpl customTechBlogPostCategoryRepository;
     private final MemberPostLogRepository memberPostLogRepository;
-    private final PostCategoryUtilityService postCategoryUtilityService;
+    private final RecommendPostExtractService recommendPostExtractService;
 
-    @Transactional
+    @Transactional(readOnly = true) // readonly ..?
     public List<Long> recommendPosts(final Long memberId, int count) {
+
         //오늘 추천은 받았으나 안 읽었던 추천 게시물 다시 가져와서 반환
         List<Long> todayUnreadRecommendPostIds = memberPostLogRepository.findTodayUnreadRecommendPostIds(memberId);
 
@@ -45,35 +36,13 @@ public class RecommendPostService {
             return todayUnreadRecommendPostIds.subList(0, requirePostCount);
         }
 
+        //새로운 서비스 클래스로 분리
         //오늘 추천은 받았으나 안 읽었던 추천 게시물은 유지하고 추가적으로 추천해줘야 하는 게시물 수를 계산
         requirePostCount -= todayUnreadRecommendPostIds.size();
 
         //카테고리_가중치 Mapping Table를 특정 MemberId로 조회
 
-        final CategoryWeights categoryWeights = new CategoryWeights(categoryWeightRepository.findByMemberId(memberId));
-
-        List<Long> categoryIds = categoryWeights.extractCategoryIds();
-
-        //카테고리별로 할당해야 하는 게시물 Map
-        //distributionMap -> key : categoryId, value : 할당해야 하는 기술블로그 개수
-        final List<ExtractedPostCategoryDto> extractedPostsCategories = customTechBlogPostCategoryRepository.findPostsByCategoryIdsNotInLog(
-                categoryIds,
-                memberId
-        );
-
-        CategoryPostDistribution distribution = CategoryPostDistribution.builder()
-                .categoryIdToPostCounts(categoryWeights.calculatePostDistribution(requirePostCount))
-                .postIds(new HashSet<>())
-                .build();
-
-
-        //추천할 게시물 ids 추출
-        Set<Long> postIds = distribution.extractRecommendPostIds(extractedPostsCategories, requirePostCount);
-
-        //오늘 추천은 받았으나 안 읽었던 추천 게시물과 추천해줘야 하는 게시물을 합쳐서 request의 count만큼 반환
-        postIds.addAll(todayUnreadRecommendPostIds);
-
-        return postIds.stream().toList();
+        return recommendPostExtractService.extractRecommendPostIds(requirePostCount, memberId, todayUnreadRecommendPostIds);
     }
 
     @Builder
