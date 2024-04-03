c6de61099ccfe112f643f3cb267505567c4b5e1a
JaehongDev <println@kakao.com>
2024-04-03 12:07:24 +0900

    test: webcleanner 추가

---
+++ drrr-batch/src/test/java/com/drrr/reader/impl/WoowahanCrawlerItemReaderTest.java
@@ -0,0 +1,14 @@
+package com.drrr.reader.impl;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class WoowahanCrawlerItemReaderTest {
+
+
+    @Disabled
+    @Test
+    void 테스트() throws Throwable {
+    }
+
+}
