4e25ed6e0d6705a524a398c8879f9368c1340cb0
YoungKyonYou <man.spride1324@gmail.com>
2024-04-08 20:05:35 +0900

    feat: 기존 버그 코드 수정, 이전코드에서 현재 날짜가 아닌 현재 날짜보다 이전 날짜가 나오는 버그 수정

--- drrr-domain/src/main/java/com/drrr/domain/log/repository/impl/CustomMemberPostLogRepositoryImpl.java
+++ drrr-domain/src/main/java/com/drrr/domain/log/repository/impl/CustomMemberPostLogRepositoryImpl.java
@@ -3,8 +3,6 @@ package com.drrr.domain.log.repository.impl;
 import static com.drrr.domain.log.entity.post.QMemberPostLog.memberPostLog;
 
 import com.drrr.domain.log.repository.CustomMemberPostLogRepository;
-import com.querydsl.core.types.dsl.DateTemplate;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDate;
 import java.util.List;
@@ -18,16 +16,13 @@ public class CustomMemberPostLogRepositoryImpl implements CustomMemberPostLogRep
 
     @Override
     public List<Long> findTodayUnreadRecommendPostIds(Long memberId) {
-        //mysql과 h2 데이터베이스 호환되게 작성
-        DateTemplate<LocalDate> date = Expressions.dateTemplate(LocalDate.class,
-                "CAST({0} AS date)", memberPostLog.createdAt);
         return queryFactory
                 .select(memberPostLog.postId)
                 .from(memberPostLog)
                 .where(memberPostLog.memberId.eq(memberId)
                         .and(memberPostLog.isRead.eq(false))
                         .and(memberPostLog.isRecommended.eq(true))
-                        .and(date.eq(LocalDate.now())))
+                        .and(memberPostLog.recommendedAt.eq(LocalDate.now())))
                 .fetch();
     }
 }
