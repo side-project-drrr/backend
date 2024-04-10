9cdde530e4a4be70cba4ae17424e181bdf4da853
YoungKyonYou <man.spride1324@gmail.com>
2024-04-10 22:59:05 +0900

    feat: Transactional 추가

--- drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalUnsignedMemberPostService.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalUnsignedMemberPostService.java
@@ -6,9 +6,11 @@ import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class ExternalUnsignedMemberPostService {
     private final TechBlogPostRepository techBlogPostRepository;
