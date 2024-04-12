5dd1fe2a8c2712b8ad3fce40d15d2671058c7791
YoungKyonYou <man.spride1324@gmail.com>
2024-04-12 22:21:54 +0900

    feat: controller에다가 view 로직 추가 및 기존 external service 제거

--- drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostViewController.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostViewController.java
@@ -1,6 +1,8 @@
 package com.drrr.techblogpost.controller;
 
-import com.drrr.techblogpost.service.ExternalUnsignedMemberPostService;
+import com.drrr.domain.exception.DomainExceptionCode;
+import com.drrr.domain.techblogpost.entity.TechBlogPost;
+import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,17 +10,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class TechBlogPostViewController {
-    private final ExternalUnsignedMemberPostService externalUnsignedMemberPostService;
+    private final TechBlogPostRepository techBlogPostRepository;
 
     @Operation(summary = "비회원이 원 게시물을 읽는 경우 조회수 증가 API"
             , description = "호출 성공 시 비회원이 조회한 기술 블로그의 조회수 증가")
@@ -28,6 +32,13 @@ public class TechBlogPostViewController {
     })
     @PostMapping("/view-post/{postId}")
     public void UnsignedMemberPostViewIncrement(@NotNull @PathVariable(name = "postId") final Long postId) {
-        externalUnsignedMemberPostService.increaseViewCount(postId);
+        final TechBlogPost post = techBlogPostRepository.findByIdWithPessimisticLock(postId)
+                .orElseThrow(() -> {
+                    log.error("기술블로그를 찾을 수 없습니다 -> postId: {}", postId);
+                    return DomainExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
+                });
+
+        //조회수 증가
+        post.increaseViewCount();
     }
 }
