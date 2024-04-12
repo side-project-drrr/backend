ac18aa38034b2f106302d0f44b7e87f13d530064
YoungKyonYou <man.spride1324@gmail.com>
2024-04-12 22:41:57 +0900

    feat: domain으로 서비스 이동

--- drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostViewController.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/controller/TechBlogPostViewController.java
@@ -1,8 +1,6 @@
 package com.drrr.techblogpost.controller;
 
-import com.drrr.domain.exception.DomainExceptionCode;
-import com.drrr.domain.techblogpost.entity.TechBlogPost;
-import com.drrr.domain.techblogpost.repository.TechBlogPostRepository;
+import com.drrr.domain.techblogpost.service.UnsignedMemberPostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class TechBlogPostViewController {
-    private final TechBlogPostRepository techBlogPostRepository;
+    private final UnsignedMemberPostService unsignedMemberPostService;
 
     @Operation(summary = "비회원이 원 게시물을 읽는 경우 조회수 증가 API"
             , description = "호출 성공 시 비회원이 조회한 기술 블로그의 조회수 증가")
@@ -32,13 +30,6 @@ public class TechBlogPostViewController {
     })
     @PostMapping("/view-post/{postId}")
     public void UnsignedMemberPostViewIncrement(@NotNull @PathVariable(name = "postId") final Long postId) {
-        final TechBlogPost post = techBlogPostRepository.findByIdWithPessimisticLock(postId)
-                .orElseThrow(() -> {
-                    log.error("기술블로그를 찾을 수 없습니다 -> postId: {}", postId);
-                    return DomainExceptionCode.TECH_BLOG_NOT_FOUND.newInstance();
-                });
-
-        //조회수 증가
-        post.increaseViewCount();
+        unsignedMemberPostService.increaseViewCount(postId);
     }
 }
