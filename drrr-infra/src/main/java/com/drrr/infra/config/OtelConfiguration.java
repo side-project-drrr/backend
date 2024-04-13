20a1f1d297f24a5dd241da6805b180b3526338e8
YoungKyonYou <man.spride1324@gmail.com>
2024-04-13 19:01:07 +0900

    feat: redis 와 otel에 host와 ip 설정 추가

--- drrr-infra/src/main/java/com/drrr/infra/config/OtelConfiguration.java
+++ drrr-infra/src/main/java/com/drrr/infra/config/OtelConfiguration.java
@@ -3,11 +3,16 @@ package com.drrr.infra.config;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.extension.trace.propagation.JaegerPropagator;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OtelConfiguration {
+    @Value("${management.otlp.endpoint}")
+    private String otlpEndpoint;
 
     @Bean
     public TextMapPropagator jaegerPropagator() {
@@ -16,6 +21,15 @@ public class OtelConfiguration {
 
     @Bean
     public OtlpGrpcSpanExporter otlpExporter() {
-        return OtlpGrpcSpanExporter.getDefault();
+        return OtlpGrpcSpanExporter.builder()
+                .setEndpoint(otlpEndpoint)
+                .build();
+    }
+
+    @Bean
+    public SdkTracerProvider tracerProvider() {
+        return SdkTracerProvider.builder()
+                .addSpanProcessor(SimpleSpanProcessor.create(otlpExporter()))
+                .build();
     }
 }
