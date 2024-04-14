3ec2a2ef2535f8ec1359ca4cf0f605785cd99107
YoungKyonYou <man.spride1324@gmail.com>
2024-04-14 11:30:03 +0900

    feat: redis 일급 컬렉션 추가

---
+++ drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/cache/entity/RedisTechBlogPostsCategoriesStaticData.java
@@ -0,0 +1,35 @@
+package com.drrr.domain.techblogpost.cache.entity;
+
+import com.drrr.domain.category.entity.RedisCategory;
+import com.drrr.domain.techblogpost.dto.TechBlogPostCategoryDto;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash(value = "redisTechBlogPostsCategoriesStaticData", timeToLive = 3600) // Redis Repository 사용을 위한
+@Builder
+public record RedisTechBlogPostsCategoriesStaticData(
+        @Id
+        Long id,
+        RedisTechBlogPostStaticData redisTechBlogPostStaticData,
+        List<RedisCategory> redisCategories
+) implements Serializable {
+
+    public static List<RedisTechBlogPostsCategoriesStaticData> from(final List<TechBlogPostCategoryDto> contents) {
+        return contents.stream()
+                .map((content) -> {
+                    RedisTechBlogPostStaticData redisTechBlogPostStaticData = RedisTechBlogPostStaticData
+                            .from(content.techBlogPostStaticDataDto());
+
+                    List<RedisCategory> redisCategories = RedisCategory.from(content.categoryDto());
+
+                    return RedisTechBlogPostsCategoriesStaticData.builder()
+                            .redisTechBlogPostStaticData(redisTechBlogPostStaticData)
+                            .redisCategories(redisCategories)
+                            .build();
+                })
+                .toList();
+    }
+}
---
+++ drrr-domain-redis/src/main/java/com/drrr/domain/techblogpost/cache/payload/RedisPostsContents.java
@@ -0,0 +1,16 @@
+package com.drrr.domain.techblogpost.cache.payload;
+
+import com.drrr.domain.category.entity.RedisCategory;
+import com.drrr.domain.techblogpost.cache.entity.RedisPostDynamicData;
+import com.drrr.domain.techblogpost.cache.entity.RedisTechBlogPostStaticData;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record RedisPostsContents(
+        RedisTechBlogPostStaticData redisTechBlogPostStaticData,
+        RedisPostDynamicData redisTechBlogPostDynamicData,
+        List<RedisCategory> redisCategories
+) {
+    
+}
