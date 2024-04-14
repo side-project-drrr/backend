0c3945748b158086026538374c97fba654514477
YoungKyonYou <man.spride1324@gmail.com>
2024-04-14 11:58:23 +0900

    feat: List 객체로 만들어주는 from 추가

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostCategoryDto.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostCategoryDto.java
@@ -5,6 +5,8 @@ import com.drrr.domain.category.dto.CategoryDto;
 import java.io.Serializable;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Builder;
 
 @Builder
@@ -21,5 +23,32 @@ public record TechBlogPostCategoryDto(
         }
         return Comparator.comparingInt((TechBlogPostCategoryDto o) -> o.techBlogPostDynamicDto().likeCount())
                 .reversed();
+    }
+
+    public static List<TechBlogPostCategoryDto> inOrderFrom(
+            final List<TechBlogPostContentDto> contents,
+            final TopTechBlogType type,
+            final Map<Long, List<CategoryDto> postIdsCategories>
+            ){
+        return contents.stream()
+                .map(content -> TechBlogPostCategoryDto.builder()
+                        .techBlogPostStaticDataDto(content.techBlogPostStaticDataDto())
+                        .categoryDto(postIdsCategories.get(content.techBlogPostStaticDataDto().id()))
+                        .techBlogPostDynamicDto(content.techBlogPostDynamicDto())
+                        .build())
+                .sorted(TechBlogPostCategoryDto.orderByTopBlogTypeCondition(type))
+                .toList();
+    }
+    public static List<TechBlogPostCategoryDto> from(
+            final List<TechBlogPostContentDto> contents,
+            final Map<Long, List<CategoryDto>> postIdsCategories
+    ) {
+        return contents.stream()
+                .map(content -> TechBlogPostCategoryDto.builder()
+                        .techBlogPostStaticDataDto(content.techBlogPostStaticDataDto())
+                        .categoryDto(postIdsCategories.get(content.techBlogPostStaticDataDto().id()))
+                        .techBlogPostDynamicDto(content.techBlogPostDynamicDto())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
