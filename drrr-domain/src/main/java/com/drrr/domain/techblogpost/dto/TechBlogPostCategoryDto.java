914b9ef9240dd179c73b98ceb8134a7aca35375d
YoungKyonYou <man.spride1324@gmail.com>
2024-04-14 11:37:07 +0900

    feat: 클래스명 수정 반영

--- drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostCategoryDto.java
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostCategoryDto.java
@@ -9,16 +9,17 @@ import lombok.Builder;
 
 @Builder
 public record TechBlogPostCategoryDto(
-        TechBlogPostBasicInfoDto techBlogPostBasicInfoDto,
+        TechBlogPostStaticDataDto techBlogPostStaticDataDto,
+        TechBlogPostDynamicDto techBlogPostDynamicDto,
         List<CategoryDto> categoryDto
 ) implements Serializable {
     public static Comparator<TechBlogPostCategoryDto> orderByTopBlogTypeCondition(final TopTechBlogType type) {
         if (type.equals(TopTechBlogType.VIEWS)) {
             //List<TechBlogPostCategoryDto>에서 ViewCount로 정렬하는 Comparator를 구현
-            return Comparator.comparingInt((TechBlogPostCategoryDto o) -> o.techBlogPostBasicInfoDto().viewCount())
+            return Comparator.comparingInt((TechBlogPostCategoryDto o) -> o.techBlogPostDynamicDto().viewCount())
                     .reversed();
         }
-        return Comparator.comparingInt((TechBlogPostCategoryDto o) -> o.techBlogPostBasicInfoDto().postLike())
+        return Comparator.comparingInt((TechBlogPostCategoryDto o) -> o.techBlogPostDynamicDto().likeCount())
                 .reversed();
     }
 }
