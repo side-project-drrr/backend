fe6e23170e1130e8d293fe1e5f39fe8d6ef906a8
YoungKyonYou <man.spride1324@gmail.com>
2024-04-14 11:30:51 +0900

    feat: jpa에서 queryprojection 할 수 있는 게시물 기본 정보를 담을 record 추가

---
+++ drrr-domain/src/main/java/com/drrr/domain/techblogpost/dto/TechBlogPostBasicInfo.java
@@ -0,0 +1,45 @@
+package com.drrr.domain.techblogpost.dto;
+
+import com.drrr.core.code.techblog.TechBlogCode;
+import com.querydsl.core.annotations.QueryProjection;
+import java.io.Serializable;
+import java.time.LocalDate;
+import lombok.Builder;
+
+@Builder
+public record TechBlogPostBasicInfo(
+        Long id,
+        String title,
+        String summary,
+        TechBlogCode techBlogCode,
+        String thumbnailUrl,
+        int viewCount,
+        int postLike,
+        LocalDate writtenAt,
+        String url
+
+) implements Serializable {
+    @QueryProjection
+    public TechBlogPostBasicInfo(
+            Long id,
+            String title,
+            String summary,
+            TechBlogCode techBlogCode,
+            String thumbnailUrl,
+            int viewCount,
+            int postLike,
+            LocalDate writtenAt,
+            String url
+    ) {
+        this.id = id;
+        this.title = title;
+        this.summary = summary;
+        this.techBlogCode = techBlogCode;
+        this.thumbnailUrl = thumbnailUrl;
+        this.viewCount = viewCount;
+        this.postLike = postLike;
+        this.writtenAt = writtenAt;
+        this.url = url;
+    }
+
+}
