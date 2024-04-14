92f1251b2b73011ab896e792ee74dc50ef63f57a
JaehongDev <println@kakao.com>
2024-04-14 22:15:57 +0900

    feat: woowahan parser 추가

---
+++ drrr-batch/src/main/java/com/drrr/parser/blog/WoowahanParser.java
@@ -0,0 +1,30 @@
+package com.drrr.parser.blog;
+
+
+import com.drrr.core.code.techblog.TechBlogCode;
+import com.drrr.fluent.cralwer.core.WebDriverPool;
+import com.drrr.parser.Parser;
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class WoowahanParser implements Parser {
+    private final WebDriverPool webDriverPool;
+
+    @Override
+    public String execute(String url) {
+        return webDriverPool.delegate(webDriver -> {
+            webDriver.get(url);
+            var by = By.className("post-content");
+            waitUnitlLoad(webDriver, by);
+            return removeUnnecessaryTag(webDriver.findElement(by).getAttribute("innerHTML"));
+        });
+    }
+
+    @Override
+    public TechBlogCode getTechBlogCode() {
+        return TechBlogCode.WOOWAHAN;
+    }
+}
