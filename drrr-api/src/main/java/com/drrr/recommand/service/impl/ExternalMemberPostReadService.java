2b0b3d379809f6fbf24f7563f404a3ae15cc95e0
YoungKyonYou <man.spride1324@gmail.com>
2024-04-15 21:51:43 +0900

    feat: 조회수 증가 서비스에 이벤트 publisher 추가

--- drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalMemberPostReadService.java
+++ drrr-api/src/main/java/com/drrr/recommand/service/impl/ExternalMemberPostReadService.java
@@ -5,12 +5,16 @@ import com.drrr.domain.category.service.MemberViewWeightService;
 import com.drrr.domain.category.service.WeightValidationService;
 import com.drrr.domain.log.service.LogUpdateService;
 import com.drrr.domain.member.entity.Member;
+import com.drrr.domain.techblogpost.event.RedisEventListener.IncreaseViewEvent;
 import com.drrr.domain.techblogpost.repository.TechBlogPostCategoryRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class ExternalMemberPostReadService {
@@ -19,8 +23,8 @@ public class ExternalMemberPostReadService {
     private final TechBlogPostCategoryRepository techBlogPostCategoryRepository;
     private final LogUpdateService logUpdateService;
     private final CategoryWeightService categoryWeightService;
+    private final ApplicationEventPublisher publisher;
 
-    @Transactional
     public void execute(final Long memberId, final Long postId) {
         //가중치 검증
         weightValidationService.validateWeight(memberId);
@@ -28,6 +32,9 @@ public class ExternalMemberPostReadService {
         List<Long> categoryIds = techBlogPostCategoryRepository.findCategoriesByPostId(postId);
         //조회수 증가
         Member member = memberViewService.increaseMemberViewPost(memberId, postId, categoryIds);
+
+        publisher.publishEvent(new IncreaseViewEvent(postId));
+
         //읽은 블로그에 대한 카테고리 가중치 증가, 카테고리의 최근 읽은 시간 업데이트
         categoryWeightService.updateCategoryWeights(categoryIds, member, postId);
         //로깅 및 히스토리 데이터 insert
