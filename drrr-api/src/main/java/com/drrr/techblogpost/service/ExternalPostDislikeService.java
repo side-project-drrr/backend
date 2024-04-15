644df9363bc3ab9f2236651fc637df60928f9b70
YoungKyonYou <man.spride1324@gmail.com>
2024-04-15 21:51:59 +0900

    feat: 좋아요 서비스에 이벤트 publisher 추가

--- drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalPostDislikeService.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalPostDislikeService.java
@@ -1,15 +1,19 @@
 package com.drrr.techblogpost.service;
 
 import com.drrr.domain.like.service.TechBlogPostLikeService;
+import com.drrr.domain.techblogpost.event.RedisEventListener.DecreaseLikeEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ExternalPostDislikeService {
     private final TechBlogPostLikeService techBlogPostLikeService;
+    private final ApplicationEventPublisher publisher;
 
     public void execute(final Long memberId, final Long postId) {
         techBlogPostLikeService.deletePostLike(memberId, postId);
+        publisher.publishEvent(new DecreaseLikeEvent(postId));
     }
 }
--- drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalPostLikeService.java
+++ drrr-api/src/main/java/com/drrr/techblogpost/service/ExternalPostLikeService.java
@@ -1,15 +1,19 @@
 package com.drrr.techblogpost.service;
 
 import com.drrr.domain.like.service.TechBlogPostLikeService;
+import com.drrr.domain.techblogpost.event.RedisEventListener.IncreaseLikeEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ExternalPostLikeService {
     private final TechBlogPostLikeService techBlogPostLikeService;
+    private final ApplicationEventPublisher publisher;
 
     public void execute(final Long memberId, final Long postId) {
         techBlogPostLikeService.addPostLike(memberId, postId);
+        publisher.publishEvent(new IncreaseLikeEvent(postId));
     }
 }
