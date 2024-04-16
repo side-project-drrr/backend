4c36f9a84c6523adfccf25855197e6663b698bd7
YoungKyonYou <man.spride1324@gmail.com>
2024-04-16 16:56:58 +0900

    feat: RedisConfig 변경

--- drrr-domain-redis/src/main/java/com/drrr/domain/config/RedisConfig.java
+++ drrr-domain-redis/src/main/java/com/drrr/domain/config/RedisConfig.java
@@ -5,23 +5,28 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration("redis configuration")
 public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setPassword("1234");
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
-    public RedisTemplate redisTemplate() {
-        final RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String,Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule()); // Java 8 날짜/시간 모듈 등록
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 날짜/시간을 timestamp로 쓰지 않도록 설정
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 날짜/시간을 timestamp로 쓰지 않도록 설
+        template.setKeySerializer(new StringRedisSerializer());
         template.setConnectionFactory(redisConnectionFactory());
 
         return template;
