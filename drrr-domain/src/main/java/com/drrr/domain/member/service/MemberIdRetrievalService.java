16b17ca3ea1a03fbee8e71c6fe7a2b50b28fd230
YoungKyonYou <man.spride1324@gmail.com>
2024-04-21 18:44:57 +0900

    test: queryservice 제공

--- drrr-domain/src/main/java/com/drrr/domain/member/service/MemberIdRetrievalService.java
+++ drrr-domain/src/main/java/com/drrr/domain/member/service/MemberIdRetrievalService.java
@@ -1,9 +1,8 @@
 package com.drrr.domain.member.service;
 
 
-import com.drrr.domain.exception.DomainExceptionCode;
 import com.drrr.domain.member.entity.Member;
-import com.drrr.domain.member.repository.MemberRepository;
+import com.drrr.domain.member.repository.common.MemberQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,18 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberIdRetrievalService {
-    private final MemberRepository memberRepository;
+    private final MemberQueryService memberQueryService;
 
     /**
      * 외부 인증의 회원 아이디 값으로 사용자를 조회함
      */
     public Long findByProviderId(final String userSocialId) {
-        final Member member = memberRepository.findByProviderId(userSocialId)
-                .orElseThrow(() -> {
-                    log.error(
-                            "MemberIdRetrievalService Class findByProviderId(final String userSocialId) Method IllegalArgumentException Error");
-                    return DomainExceptionCode.UNREGISTERED_MEMBER.newInstance();
-                });
+        final Member member = memberQueryService.getMemberByProviderId(userSocialId);
         return member.getId();
     }
 
