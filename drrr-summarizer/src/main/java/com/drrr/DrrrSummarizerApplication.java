0fa389450afcae1e1bffedd2bdd71a9afd4f8dc5
JaehongDev <println@kakao.com>
2024-04-24 00:08:38 +0900

    feat: PostContentsSummarizer 모듈 추가

---
+++ drrr-summarizer/src/main/java/com/drrr/DrrrSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.drrr;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrrrSummarizerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(DrrrSummarizerApplication.class, args);
+    }
+}
