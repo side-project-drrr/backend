179644c9888f46229dd6d071d02cf7f043613908
YoungKyonYou <man.spride1324@gmail.com>
2024-05-04 13:44:02 +0900

    test: test 프로파일 활성화

--- drrr-api/src/test/java/com/drrr/recommend/RecommendServiceE2ETest.java
+++ drrr-api/src/test/java/com/drrr/recommend/RecommendServiceE2ETest.java
@@ -43,7 +43,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RecommendServiceE2ETest {
 
