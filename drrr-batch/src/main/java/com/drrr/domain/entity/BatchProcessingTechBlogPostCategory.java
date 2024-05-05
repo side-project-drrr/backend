e16670e16de39906e62e3f61b094f7dcb2338097
JaehongDev <println@kakao.com>
2024-05-05 14:21:00 +0900

    feat: 카테고리 이름 정규화 판별 기능 구현

--- drrr-batch/src/main/java/com/drrr/domain/entity/BatchProcessingTechBlogPostCategory.java
+++ drrr-batch/src/main/java/com/drrr/domain/entity/BatchProcessingTechBlogPostCategory.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Table(name = "DRRR_TECHBLOGPOST_CATEGORY")
 public class BatchProcessingTechBlogPostCategory extends BaseEntity {
 
-    private static final String REPLACE_FAILE_MESSAGE_FORMAT = "잘못된 reference id입니다. category id: %s, post id : %s";
+    private static final String REPLACE_FAIL_MESSAGE_FORMAT = "잘못된 reference id입니다. category id: %s, post id : %s";
 
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -33,33 +33,50 @@ public class BatchProcessingTechBlogPostCategory extends BaseEntity {
     @JoinColumn(name = "category_id")
     private Category category;
 
+    public BatchProcessingTechBlogPostCategory(BatchProcessingTechBlog batchProcessingTechBlog, Category category) {
+        this.post = batchProcessingTechBlog;
+        this.category = category;
+    }
+
 
     public Long getReferenceId() {
         return category.getReferenceId();
     }
 
-    public Long getCategoryId() {
-        return category.getId();
-    }
-
-    public Long getPostId() {
-        return post.getId();
-    }
-
-    public void replaceCategory(
-            Function<Long, Optional<Category>> categoryReplacer
-    ) {
-        if (category.isReplaceType()) {
-            log.info("replace 처리");
-
-            categoryReplacer.apply(getReferenceId())
-                    .orElseThrow(() -> new IllegalStateException(String.format(REPLACE_FAILE_MESSAGE_FORMAT,
-                                    getCategory(),
-                                    getReferenceId())
-                            )
-                    );
+    public void replaceCategory(Function<Long, Optional<Category>> categoryReplacer) {
+        if (!category.isReplaceType()) {
+            return;
         }
+
+        // 카테고리 변환이 실패하는 경우 reference id가 잘못된 상태
+        this.category = categoryReplacer.apply(getReferenceId())
+                .orElseThrow(() ->
+                        new IllegalStateException(
+                                String.format(REPLACE_FAIL_MESSAGE_FORMAT, getCategory(), getReferenceId()))
+                );
     }
 
 
+    /**
+     * 정규화되지 않은 카테고리 이름 여부를 판단하는 메서드
+     *
+     * <p>
+     * 정상적인 카테고리 명 ex) "ELK (ElasticSearch, k...)"
+     * <p>
+     * 비정상적인 카테고리 명 ex) "Java, Node, Spring"
+     */
+    public boolean isUnNormalizedCategoryName() {
+        var categoryName = this.getCategoryName();
+
+        boolean containsComma = categoryName.contains(",");
+        boolean containsParentheses = categoryName.contains("(") && categoryName.contains(")");
+        boolean commaAfterParentheses = containsParentheses && categoryName.indexOf(",") > categoryName.indexOf(")");
+
+        return containsComma && (!containsParentheses || commaAfterParentheses);
+    }
+
+
+    public String getCategoryName() {
+        return category.getName();
+    }
 }
