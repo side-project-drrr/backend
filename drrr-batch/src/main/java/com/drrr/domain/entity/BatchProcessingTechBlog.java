88e7d3657161380f38ae3ab1ed550fc309b49f61
JaehongDev <println@kakao.com>
2024-05-05 14:23:44 +0900

    feat: 카테고리 이름 재처리 기능 구현

--- drrr-batch/src/main/java/com/drrr/domain/entity/BatchProcessingTechBlog.java
+++ drrr-batch/src/main/java/com/drrr/domain/entity/BatchProcessingTechBlog.java
@@ -7,12 +7,16 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,16 +34,63 @@ public class BatchProcessingTechBlog extends BaseEntity {
     private List<BatchProcessingTechBlogPostCategory> categories;
 
 
+    @Transient
+    private List<BatchProcessingTechBlogPostCategory> unNormalizedPostCategory;
+
     private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
-        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) != null;
     }
 
     public void replaceCategories(Function<Long, Optional<Category>> categoryReplacer) {
         categories.forEach(postCategory -> postCategory.replaceCategory(categoryReplacer));
-        categories.removeIf(
-                distinctByKey(postCategory -> postCategory.getPostId() + ":" + postCategory.getCategoryId()));
+        categories.removeIf(distinctByKey(BatchProcessingTechBlogPostCategory::getCategoryName));
     }
 
 
+    public List<Category> executeNormalize() {
+        final var unNormalizedPostCategory = categories.stream()
+                .filter(BatchProcessingTechBlogPostCategory::isUnNormalizedCategoryName)
+                .toList();
+
+        if (unNormalizedPostCategory.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        log.info("정규화된 처리 전 카테고리 이름 {}",
+                unNormalizedPostCategory.stream()
+                        .map(BatchProcessingTechBlogPostCategory::getCategoryName)
+                        .collect(Collectors.joining("\n"))
+        );
+
+        this.unNormalizedPostCategory = unNormalizedPostCategory;
+
+        return unNormalizedPostCategory.stream()
+                .map(BatchProcessingTechBlogPostCategory::getCategoryName)
+                .map(str -> str.split(","))
+                .flatMap(names -> Arrays.stream(names)
+                        .map(String::trim)
+                        .map(Category::new)
+                )
+                .toList();
+    }
+
+    /**
+     * 새로운 카테고리 등록하면서 기존 정규화되지 않은 카테고리를 삭제합니다.
+     *
+     * @param newCategories
+     */
+
+    public void register(
+            List<Category> newCategories
+    ) {
+
+        categories.removeAll(unNormalizedPostCategory);
+
+        newCategories.stream()
+                .map(category -> new BatchProcessingTechBlogPostCategory(this, category))
+                .forEach(categories::add);
+
+        this.categories.removeIf(distinctByKey(BatchProcessingTechBlogPostCategory::getCategoryName));
+    }
 }
