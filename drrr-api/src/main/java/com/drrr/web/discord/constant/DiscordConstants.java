8e49c33f17ed4c755254b132d4a5274cde02414d
YoungKyonYou <man.spride1324@gmail.com>
2024-05-12 19:48:48 +0900

    feat: 디스코드 상수 추가

---
+++ drrr-api/src/main/java/com/drrr/web/discord/constant/DiscordConstants.java
@@ -0,0 +1,24 @@
+package com.drrr.web.discord.constant;
+
+
+public class DiscordConstants {
+
+    public static final String EXCEPTION_NOT_FOUND = "EXCEPTION 정보가 남지 않았습니다.";
+    public static final String WEBHOOK_URL = "webhookUrl";
+    public static final String REQUEST_URI = "requestUri";
+    public static final String METHOD = "method";
+    public static final String HEADERS = "headers";
+    public static final String PARAMS = "params";
+    public static final String BODY = "body";
+    public static final String IP_ADDRESS = "ipAddress";
+    public static final String REQUEST_URI_MDC = "이용자 요청 URI 정보";
+    public static final String USER_IP_MDC = "이용자 IP 정보";
+    public static final String HEADER_MAP_MDC = "HTTP 헤더 정보";
+    public static final String PARAMETER_MAP_MDC = "Parameter 정보";
+    public static final String BODY_MDC = "HTTP Body 정보";
+    public static final String ERROR_OCCUR_TIME_MDC = "문제 발생 시각";
+    public static final String EXCEPTION_LEVEL_MDC = "Exception Level";
+    public static final String BRIEF_EXCEPTION_MDC = "문제 간략 내용";
+    public static final String EXCEPTION_DETAIL_MDC = "문제 상세 내용";
+
+}
