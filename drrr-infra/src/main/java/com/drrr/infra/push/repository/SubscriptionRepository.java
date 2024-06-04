46e4e4e814fbb6818f4db99c46b0c46326eca0d9
YoungKyonYou <man.spride1324@gmail.com>
2024-06-04 23:37:18 +0900

    feat: 구독정보 엔티티 변경 및 rate 적용할 때 healthcheck는 무시

--- drrr-infra/src/main/java/com/drrr/infra/push/repository/SubscriptionRepository.java
+++ drrr-infra/src/main/java/com/drrr/infra/push/repository/SubscriptionRepository.java
@@ -3,13 +3,12 @@ package com.drrr.infra.push.repository;
 
 import com.drrr.infra.push.entity.Subscription;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, String> {
 
     @Modifying
     void deleteByMemberId(final Long memberId);
